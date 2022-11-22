3eb8a7072e50d0a69a5a9f3078459d0d9078c48d
zoruka <zo.fmendes@gmail.com>
2022-11-22 17:46:45 -0300

    refactor: implement interface in the contract file

--- contracts/SitesNFTs.sol
+++ contracts/SitesNFTs.sol
@@ -5,8 +5,9 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "../interfaces/ISitesNFTs.sol";
 
-contract SitesNFTs is ERC721URIStorage, AccessControl {
+contract SitesNFTs is ISitesNFTs, ERC721URIStorage, AccessControl  {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -30,7 +31,7 @@ contract SitesNFTs is ERC721URIStorage, AccessControl {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function mint(string memory base64EncodedMetadata, address account) public canMint()  returns (uint256) {
+    function mint(string memory base64EncodedMetadata, address account) public canMint override returns (uint256) {
         uint256 newItemId = _tokenIds.current();
         _safeMint(account, newItemId);
         _setTokenURI(newItemId, base64EncodedMetadata);
@@ -39,30 +40,26 @@ contract SitesNFTs is ERC721URIStorage, AccessControl {
         return newItemId;
     }
 
-    function updateTokenURI(address tokenHolderAddress, uint256 tokenId, string memory base64EncodedMetadata) public canMint() {
+    function updateTokenURI(address tokenHolderAddress, uint256 tokenId, string memory base64EncodedMetadata) public canMint override {
         address tokenOwner = ownerOf(tokenId);
         require(tokenOwner == tokenHolderAddress, "Address does not own provided tokenId");
         _setTokenURI(tokenId, base64EncodedMetadata);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC721, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
-    function setBaseURI(string memory _newBbaseURI) public canChangeBaseURI() returns (string memory) {
+    function setBaseURI(string memory _newBbaseURI) public canChangeBaseURI override returns (string memory) {
         baseURI = _newBbaseURI;
         return baseURI;
     }
 
-    function getCurrentTokenId() public view returns (uint256) {
+    function getCurrentTokenId() public view override returns (uint256) {
         return _tokenIds.current();
     }
 
     function _baseURI() internal view override returns (string memory) {
         return baseURI;
     }
-
-    receive() external payable {}
-
-    fallback() external {}
 }
