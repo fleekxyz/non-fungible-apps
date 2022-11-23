80a0ae98c174cb15e40c0e78dfe02f3af696b965
Felipe Mendes <zo.fmendes@gmail.com>
2022-11-23 09:09:39 -0300

    refactor: repo structure (#1)
    * chore: update .gitignore folders and add testing script
    
    * feat: sites nft interface
    
    * refactor: implement interface in the contract file
    
    * chore: apply code formatting

--- contracts/SitesNFTs.sol
+++ contracts/SitesNFTs.sol
@@ -1,21 +1,23 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "../interfaces/ISitesNFTs.sol";
 
-contract SitesNFTs is ERC721URIStorage, AccessControl {
-
+contract SitesNFTs is ISitesNFTs, ERC721URIStorage, AccessControl {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     string private baseURI;
 
-    bytes32 public constant MINTER_ROLE = 0x4d494e5445525f524f4c45000000000000000000000000000000000000000000; // "MINTER_ROLE"
+    bytes32 public constant MINTER_ROLE =
+        0x4d494e5445525f524f4c45000000000000000000000000000000000000000000; // "MINTER_ROLE"
 
     modifier canMint() {
-        bool isMinterOrAdmin = hasRole(MINTER_ROLE, msg.sender) || hasRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        bool isMinterOrAdmin = hasRole(MINTER_ROLE, msg.sender) ||
+            hasRole(DEFAULT_ADMIN_ROLE, msg.sender);
         require(isMinterOrAdmin, "Caller has no permission to mint.");
         _;
     }
@@ -30,7 +32,10 @@ contract SitesNFTs is ERC721URIStorage, AccessControl {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function mint(string memory base64EncodedMetadata, address account) public canMint()  returns (uint256) {
+    function mint(
+        string memory base64EncodedMetadata,
+        address account
+    ) public override canMint returns (uint256) {
         uint256 newItemId = _tokenIds.current();
         _safeMint(account, newItemId);
         _setTokenURI(newItemId, base64EncodedMetadata);
@@ -39,30 +44,43 @@ contract SitesNFTs is ERC721URIStorage, AccessControl {
         return newItemId;
     }
 
-    function updateTokenURI(address tokenHolderAddress, uint256 tokenId, string memory base64EncodedMetadata) public canMint() {
+    function updateTokenURI(
+        address tokenHolderAddress,
+        uint256 tokenId,
+        string memory base64EncodedMetadata
+    ) public override canMint {
         address tokenOwner = ownerOf(tokenId);
-        require(tokenOwner == tokenHolderAddress, "Address does not own provided tokenId");
+        require(
+            tokenOwner == tokenHolderAddress,
+            "Address does not own provided tokenId"
+        );
         _setTokenURI(tokenId, base64EncodedMetadata);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        virtual
+        override(IERC165, ERC721, AccessControl)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 
-    function setBaseURI(string memory _newBbaseURI) public canChangeBaseURI() returns (string memory) {
+    function setBaseURI(
+        string memory _newBbaseURI
+    ) public override canChangeBaseURI returns (string memory) {
         baseURI = _newBbaseURI;
         return baseURI;
     }
 
-    function getCurrentTokenId() public view returns (uint256) {
+    function getCurrentTokenId() public view override returns (uint256) {
         return _tokenIds.current();
     }
 
     function _baseURI() internal view override returns (string memory) {
         return baseURI;
     }
-
-    receive() external payable {}
-
-    fallback() external {}
 }
