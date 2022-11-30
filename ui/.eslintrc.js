689c826bc1a1921d71c0aed580425855491a511f
Camila Sosa Morales <camisosa@fleek.co>
2022-11-30 17:19:25 -0300

    feat: boilerplate for UI

---
+++ ui/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      { allowExpressions: true },
+    ],
+    '@typescript-eslint/no-namespace': 'off',
+    'simple-import-sort/imports': 2,
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'no-console': 'error',
+  },
+};
---
+++ ui/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="main">
+      <h1>Welcome to Sites as NFTs by Fleek</h1>
+    </div>
+  );
+};
+
+export default App;
