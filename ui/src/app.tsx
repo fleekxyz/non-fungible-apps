f85b50de7533c026e1cac6069ac53cd866b5998a
Camila Sosa Morales <camisosa@fleek.co>
2022-11-30 19:58:36 -0300

    refactor: rename files. change default export for named export

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
+export const App = () => {
   return (
     <div className="main">
       <h1>Welcome to Sites as NFTs by Fleek</h1>
     </div>
   );
 };
-
-export default App;
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,3 +12,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
