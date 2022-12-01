c9e3102405fe0e7cf3f2108a56db605f7d70ac34
Camila Sosa Morales <camisosa@fleek.co>
2022-12-01 17:25:17 -0300

    feat: add formik for form validation

--- ui/src/views/mint-site/mint.tsx
+++ ui/src/views/mint-site/mint.tsx
@@ -8,6 +8,7 @@ import {
   Input,
   Button,
 } from '@chakra-ui/react';
+import { Formik, Field } from 'formik';
 
 export const MintSite = () => {
   return (
@@ -18,31 +19,88 @@ export const MintSite = () => {
             <Heading>Mint your Site</Heading>
           </Box>
           <Box my={4} textAlign="left">
-            <form>
-              <FormControl isRequired>
-                <FormLabel>Github commit (org, repo, commit)</FormLabel>
-                <Input />
-              </FormControl>
-              <FormControl mt={6} isRequired>
-                <FormLabel>Owner address</FormLabel>
-                <Input />
-              </FormControl>
-              <FormControl mt={6}>
-                <FormLabel>Controller address</FormLabel>
-                <Input />
-              </FormControl>
-              <FormControl mt={6} isRequired>
-                <FormLabel>IPFS Hash</FormLabel>
-                <Input />
-              </FormControl>
-              <FormControl mt={6} isRequired>
-                <FormLabel>ENS</FormLabel>
-                <Input />
-              </FormControl>
-              <Button width="full" mt={4} type="submit">
-                Mint
-              </Button>
-            </form>
+            <Formik
+              initialValues={{
+                githubCommit: '',
+                ownerAddress: '',
+                controllerAddress: '',
+                ipfsHash: '',
+                ens: '',
+              }}
+            >
+              {({ values, touched, handleSubmit, isSubmitting }) => (
+                <form onSubmit={handleSubmit}>
+                  <FormControl
+                    isRequired
+                    isInvalid={touched.githubCommit && !values.githubCommit}
+                  >
+                    <FormLabel htmlFor="githubCommit">
+                      Github commit (org, repo, commit)
+                    </FormLabel>
+                    <Field
+                      as={Input}
+                      name="githubCommit"
+                      id="githubCommit"
+                      type="text"
+                    />
+                  </FormControl>
+                  <FormControl
+                    mt={6}
+                    isRequired
+                    isInvalid={touched.ownerAddress && !values.ownerAddress}
+                  >
+                    <FormLabel htmlFor="ownerAddress">Owner address</FormLabel>
+                    <Field
+                      as={Input}
+                      name="ownerAddress"
+                      id="ownerAddress"
+                      type="text"
+                    />
+                  </FormControl>
+                  <FormControl mt={6}>
+                    <FormLabel>Controller address</FormLabel>
+                    <Input type="text" />
+                  </FormControl>
+                  <FormControl
+                    mt={6}
+                    isRequired
+                    isInvalid={touched.ipfsHash && !values.ipfsHash}
+                  >
+                    <FormLabel htmlFor="ipfsHash">IPFS Hash</FormLabel>
+                    <Field
+                      as={Input}
+                      name="ipfsHash"
+                      id="ipfsHash"
+                      type="ipfsHash"
+                    />
+                  </FormControl>
+                  <FormControl
+                    mt={6}
+                    isRequired
+                    isInvalid={touched.ens && !values.ens}
+                  >
+                    <FormLabel htmlFor="ens">ENS</FormLabel>
+                    <Field as={Input} name="ens" id="ens" type="ens" />
+                  </FormControl>
+                  <Button
+                    width="full"
+                    mt={4}
+                    type="submit"
+                    disabled={
+                      isSubmitting ||
+                      !(
+                        !!values.githubCommit &&
+                        !!values.ownerAddress &&
+                        !!values.ipfsHash &&
+                        !!values.ens
+                      )
+                    }
+                  >
+                    Mint
+                  </Button>
+                </form>
+              )}
+            </Formik>
           </Box>
         </Box>
       </Flex>
