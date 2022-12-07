07ad6b672afbc8fe64a9b8cb8327348aed410d46
zoruka <zo.fmendes@gmail.com>
2022-12-07 15:35:35 -0300

    feat: add interaction scripts

---
+++ scripts/tokenURI.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+// TODO: make this arguments
+const contractAddress = '0x91A425C1CA320A99a09BE1bee114Fce5d30153d9';
+const tokenId = 3;
+
+(async () => {
+  const contract = await hre.ethers.getContractAt(
+    'FleekERC721',
+    contractAddress
+  );
+
+  const transaction = await contract.tokenURI(tokenId);
+
+  const parsed = JSON.parse(
+    Buffer.from(transaction.slice(29), 'base64').toString('utf-8')
+  );
+
+  console.log('Response: ', parsed);
+})();
---
+++ scripts/upgrade.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+// TODO: make this arguments
+const contractAddress = '0x91A425C1CA320A99a09BE1bee114Fce5d30153d9';
+const params = [
+  3, // tokenId
+  '97e7908f70f0862d753c66689ff09e70caa43df2', // commit hash
+  'https://github.com/org/new-repo', // repo
+  'new-author', // author
+];
+
+(async () => {
+  const contract = await hre.ethers.getContractAt(
+    'FleekERC721',
+    contractAddress
+  );
+
+  const transaction = await contract.upgradeTokenBuild(...params);
+
+  console.log('Response: ', transaction);
+})();
