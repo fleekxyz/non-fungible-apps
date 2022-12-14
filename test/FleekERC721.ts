776577582e8c0d49ddfa5e874cd0697b8941d481
zoruka <zo.fmendes@gmail.com>
2022-12-14 10:22:10 -0300

    test: add access control role event emit tests

--- test/FleekERC721.ts
+++ test/FleekERC721.ts
@@ -386,6 +386,43 @@ describe('FleekERC721', () => {
           .grantTokenRole(tokenId, ROLES.CONTROLLER, otherAccount.address)
       ).to.not.be.reverted;
     });
+
+    it('should emit event when token role is granted', async () => {
+      const { contract, owner, otherAccount } = fixture;
+      await expect(
+        contract.grantTokenRole(tokenId, ROLES.CONTROLLER, otherAccount.address)
+      )
+        .to.emit(contract, 'TokenRoleGranted')
+        .withArgs(
+          tokenId,
+          ROLES.CONTROLLER,
+          otherAccount.address,
+          owner.address
+        );
+    });
+
+    it('should emit event when token role is revoked', async () => {
+      const { contract, owner, otherAccount } = fixture;
+      await contract.grantTokenRole(
+        tokenId,
+        ROLES.CONTROLLER,
+        otherAccount.address
+      );
+      await expect(
+        contract.revokeTokenRole(
+          tokenId,
+          ROLES.CONTROLLER,
+          otherAccount.address
+        )
+      )
+        .to.emit(contract, 'TokenRoleRevoked')
+        .withArgs(
+          tokenId,
+          ROLES.CONTROLLER,
+          otherAccount.address,
+          owner.address
+        );
+    });
   });
 
   describe('Collection Roles', () => {
@@ -528,6 +565,31 @@ describe('FleekERC721', () => {
           .connect(otherAccount)
           .revokeCollectionRole(ROLES.OWNER, owner.address)
       ).to.be.revertedWith('FleekAccessControl: must have collection role');
+    });
+
+    it('should emit event when role is granted', async () => {
+      const { owner, contract, otherAccount } = fixture;
+
+      await expect(
+        contract.grantCollectionRole(ROLES.CONTROLLER, otherAccount.address)
+      )
+        .to.emit(contract, 'CollectionRoleGranted')
+        .withArgs(ROLES.CONTROLLER, otherAccount.address, owner.address);
+    });
+
+    it('should emit event when role is revoked', async () => {
+      const { owner, contract, otherAccount } = fixture;
+
+      await contract.grantCollectionRole(
+        ROLES.CONTROLLER,
+        otherAccount.address
+      );
+
+      await expect(
+        contract.revokeCollectionRole(ROLES.CONTROLLER, otherAccount.address)
+      )
+        .to.emit(contract, 'CollectionRoleRevoked')
+        .withArgs(ROLES.CONTROLLER, otherAccount.address, owner.address);
     });
   });
 });
