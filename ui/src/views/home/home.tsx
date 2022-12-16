d96175fa3f92bd11985eaa979d13e53cc8821ac3
Camila Sosa Morales <camisosa@fleek.co>
2022-12-16 11:27:39 -0300

    feat: list minted sites (#42)
    * feat: add mintes sites list view
    
    * wip: add card but need styles
    
    * style: add style to site card component
    
    * style: add style to site card
    
    * fix: fix onClick propagation
    
    * chore: remove unused param
    
    * Apply suggestions from code review
    
    Co-authored-by: Felipe Mendes <zo.fmendes@gmail.com>
    
    * Update list.tsx
    
    Co-authored-by: Felipe Mendes <zo.fmendes@gmail.com>

--- ui/src/views/home/home.tsx
+++ ui/src/views/home/home.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Heading, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { Flex } from '@chakra-ui/react';
+import { ListSites } from './list';
 
 export const Home = () => {
   return (
     <Flex flexDirection="column" alignItems="center">
-      <Heading>Welcome to Sites as NFTs by Fleek</Heading>
-      {/* TODO add list sites */}
-      <Button as={Link} to="/mint-site" mt={10}>
+      <Heading marginTop="80px">Welcome to Sites as NFTs by Fleek</Heading>
+      <Button as={Link} to="/mint-site" mt="20px" mb="50px">
         Mint your site
       </Button>
+      <ListSites />
     </Flex>
   );
 };
