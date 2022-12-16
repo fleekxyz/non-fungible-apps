5d14d517ae2780ee19dede9a751c44995eb47589
Camila Sosa Morales <camisosa@fleek.co>
2022-12-16 16:31:38 -0300

    reactor: refactor on useToast

--- ui/src/hooks/use-toast.ts
+++ ui/src/hooks/use-toast.ts
@@ -1,22 +1,9 @@
-import { useToast as useToastChakra, UseToastOptions } from '@chakra-ui/react';
-import { useCallback } from 'react';
+import { useToast as useToastChakra } from '@chakra-ui/react';
 
 export const useToast = () => {
-  const toast = useToastChakra();
-  return useCallback(
-    ({
-      duration = 3000,
-      position = 'buttom' as UseToastOptions['position'],
-      isClosable = true,
-      ...params
-    }: UseToastOptions) =>
-      toast({
-        duration,
-        position: position as UseToastOptions['position'],
-        isClosable,
-        ...params,
-      }) as unknown as typeof toast,
-    []
-  );
+  return useToastChakra({
+    duration: 3000,
+    isClosable: true,
+  });
 };
 
