0194d0c07ad5b9d9be313bb6807f33110d889906
zoruka <zo.fmendes@gmail.com>
2022-12-20 12:20:52 -0300

    fix: list view items displaying

--- ui/src/views/home/list.tsx
+++ ui/src/views/home/list.tsx
@@ -1,5 +1,5 @@
 import { Loading } from '@/components';
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import { useQuery } from 'react-query';
 import { SiteCard } from '@/components';
 import { FleekERC721 } from '@/integrations';
@@ -11,20 +11,12 @@ export const ListSites = () => {
 
   if (!data || isLoading) return <Loading />;
   return (
-    <Grid
-      templateColumns={{ base: 'repeat(4, 1fr)', md: 'repeat(5, 1fr)' }}
-      gap={10}
-      mt="40px"
-    >
+    <Flex gap={10} mt="40px" flexWrap="wrap" justifyContent="center">
       {new Array(data).fill(0).map((_, index) => {
         const id = data - index - 1;
-        return (
-          <GridItem key={id}>
-            <SiteCard tokenId={id} />
-          </GridItem>
-        );
+        return <SiteCard tokenId={id} />;
       })}
-    </Grid>
+    </Flex>
   );
 };
 
