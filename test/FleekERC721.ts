69de99467ff278036aadcde22b420b92ffdb6c8c
EmperorOrokuSaki <artie.eth@gmail.com>
2023-01-11 18:55:51 +0330

    Fix the HardHat decoding bug by adding base URI to _generateSVG() and updating foundry tests

--- test/FleekERC721.ts
+++ test/FleekERC721.ts
@@ -139,7 +139,7 @@ describe('FleekERC721', () => {
     });
 
     it('should return the token URI', async () => {
-      /**const { contract } = fixture;
+      const { contract } = fixture;
       const tokenURI = await contract.tokenURI(tokenId);
 
       const tokenURIDecoded = Buffer.from(
@@ -149,28 +149,35 @@ describe('FleekERC721', () => {
 
       const parsedURI = JSON.parse(tokenURIDecoded);
 
-      console.log(parsedURI.image);
+      const imageDecoded = Buffer.from(
+        parsedURI.image.replace('data:application/json;base64,', ''),
+        'base64'
+      ).toString('ascii');
+
+      parsedURI.image = imageDecoded;
 
       expect(parsedURI).to.eql({
         owner: fixture.owner.address.toLowerCase(),
         name: MINT_PARAMS.name,
         description: MINT_PARAMS.description,
-        image: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="640" height="480" viewBox="0 0 640 480" xml:space="preserve">'+"<defs>"+
-        +"</defs>"
-        +'<g transform="matrix(3.42 0 0 3.42 300.98 252.98)"  >'
-        +'<polygon style="stroke: rgb(0,0,0); stroke-width: 8; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(152,152,183); fill-rule: nonzero; opacity: 1;" vector-effect="non-scaling-stroke"  points="-50,-50 -50,50 50,50 50,-50 " />'
-        +"</g>"
-        +'<g transform="matrix(1 0 0 1 303.5 115.67)" style=""  >'
-        +'<text xml:space="preserve" font-family="Open Sans" font-size="24" font-style="normal" font-weight="normal" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1; white-space: pre;" ><tspan x="-45.7" y="5.65" style="stroke-width: 1; font-family: "Open Sans", sans-serif; font-size: 18px; font-style: normal; font-weight: normal; fill: rgb(0,0,0); ">Fleek NFAs</tspan></text>'
-        +"</g>"
-        +'<g transform="matrix(1 0 0 1 302 261.47)" style=""  >'
-        +'<text xml:space="preserve" font-family="Open Sans" font-size="28" font-style="normal" font-weight="normal" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1; white-space: pre;" ><tspan x="-44.26" y="-6.14" style="stroke-width: 1; font-family: "Open Sans", sans-serif; font-size: 18px; font-style: normal; font-weight: normal; fill: rgb(0,0,0); ">'
-        +MINT_PARAMS.name
-        +'</tspan><tspan x="-37.14" y="17.45" style="stroke-width: 1; font-family: "Open Sans", sans-serif; font-size: 18px; font-style: normal; font-weight: normal; fill: rgb(0,0,0); ">'
-        +MINT_PARAMS.ens
-        +"</tspan></text>"
-        +"</g>"
-        +"</svg>",
+        image:
+          '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="640" height="480" viewBox="0 0 640 480" xml:space="preserve">' +
+          '<defs>' +
+          '</defs>' +
+          '<g transform="matrix(3.42 0 0 3.42 300.98 252.98)"  >' +
+          '<polygon style="stroke: rgb(0,0,0); stroke-width: 8; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(152,152,183); fill-rule: nonzero; opacity: 1;" vector-effect="non-scaling-stroke"  points="-50,-50 -50,50 50,50 50,-50 " />' +
+          '</g>' +
+          '<g transform="matrix(1 0 0 1 303.5 115.67)" style=""  >' +
+          '<text xml:space="preserve" font-family="Open Sans" font-size="24" font-style="normal" font-weight="normal" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1; white-space: pre;" ><tspan x="-45.7" y="5.65" style="stroke-width: 1; font-family: "Open Sans", sans-serif; font-size: 18px; font-style: normal; font-weight: normal; fill: rgb(0,0,0); ">Fleek NFAs</tspan></text>' +
+          '</g>' +
+          '<g transform="matrix(1 0 0 1 302 261.47)" style=""  >' +
+          '<text xml:space="preserve" font-family="Open Sans" font-size="28" font-style="normal" font-weight="normal" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1; white-space: pre;" ><tspan x="-44.26" y="-6.14" style="stroke-width: 1; font-family: "Open Sans", sans-serif; font-size: 18px; font-style: normal; font-weight: normal; fill: rgb(0,0,0); ">' +
+          MINT_PARAMS.name +
+          '</tspan><tspan x="-37.14" y="17.45" style="stroke-width: 1; font-family: "Open Sans", sans-serif; font-size: 18px; font-style: normal; font-weight: normal; fill: rgb(0,0,0); ">' +
+          MINT_PARAMS.ens +
+          '</tspan></text>' +
+          '</g>' +
+          '</svg>',
         external_url: MINT_PARAMS.externalUrl,
         attributes: [
           {
@@ -190,7 +197,7 @@ describe('FleekERC721', () => {
             value: '0',
           },
         ],
-      });**/
+      });
     });
   });
 
