92fa27096964759b5e48d7aadc6742d1c2940f44
Camila Sosa Morales <camisosa@fleek.co>
2023-01-17 12:06:51 -0500

    style: dark mode

--- ui/.storybook/main.js
+++ ui/.storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    'storybook-dark-mode',
   ],
   framework: '@storybook/react',
   core: {
