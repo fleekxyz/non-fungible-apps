14f3dd441f7bc3c4ffa40d8bd59b81471e119157
zoruka <zo.fmendes@gmail.com>
2023-01-23 11:35:15 -0300

    feat: add library deployment storage

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,58 @@
-const { deployStore } = require('./utils/deploy-store');
+const {
+  deployStore,
+  getCurrentAddressIfSameBytecode,
+} = require('./utils/deploy-store');
 const { getProxyAddress, proxyStore } = require('./utils/proxy-store');
 
-const CONTRACT_NAME = 'FleekERC721';
-const NETWORK = hre.network.name;
+// --- Inputs ---
 const ARGUMENTS = [
   'FleekNFAs', // Collection name
   'FLKNFA', // Collection symbol
 ];
 
-const deploy = async () => {
-  const Contract = await ethers.getContractFactory(CONTRACT_NAME);
+// --- Script Settings ---
+const CONTRACT_NAME = 'FleekERC721';
+const NETWORK = hre.network.name;
+const DEFAULT_PROXY_SETTINGS = {
+  unsafeAllow: ['external-library-linking'],
+};
+const LIBRARIES_TO_DEPLOY = ['FleekSVG'];
+
+const libraryDeployment = async () => {
+  console.log('Deploying Libraries...');
+  const libraries = {};
+
+  for (const lib of LIBRARIES_TO_DEPLOY) {
+    const libAddress = await getCurrentAddressIfSameBytecode(lib);
+    if (libAddress) {
+      console.log(`Library "${lib}" already deployed at ${libAddress}`);
+      libraries[lib] = libAddress;
+      continue;
+    }
+    const libContract = await hre.ethers.getContractFactory(lib);
+    const libInstance = await libContract.deploy();
+    await libInstance.deployed();
+    deployStore(NETWORK, lib, libInstance);
+    console.log(`Library "${lib}" deployed at ${libInstance.address}`);
+    libraries[lib] = libInstance.address;
+  }
+
+  // return {
+  //   FleekSVG: '0xcee2D4448237286CBca736d42800d9D59b92002c',
+  // };
+  return libraries;
+};
+
+const main = async () => {
+  console.log(':: Starting Deployment ::');
+  console.log('Network:', NETWORK);
+  console.log('Contract:', CONTRACT_NAME);
+
+  const libraries = await libraryDeployment();
+
+  const Contract = await ethers.getContractFactory(CONTRACT_NAME, {
+    libraries,
+  });
   const proxyAddress = await getProxyAddress(CONTRACT_NAME, NETWORK);
 
   let deployResult;
@@ -17,7 +60,11 @@ const deploy = async () => {
   try {
     if (!proxyAddress) throw new Error('No proxy address found');
     console.log(`Trying to upgrade proxy contract at: "${proxyAddress}"`);
-    deployResult = await upgrades.upgradeProxy(proxyAddress, Contract);
+    deployResult = await upgrades.upgradeProxy(
+      proxyAddress,
+      Contract,
+      DEFAULT_PROXY_SETTINGS
+    );
     console.log(
       `Contract ${CONTRACT_NAME} upgraded at "${deployResult.address}" by account "${deployResult.signer.address}"`
     );
@@ -29,7 +76,11 @@ const deploy = async () => {
     ) {
       console.log(`Failed to upgrade proxy contract: "${e.message?.trim()}"`);
       console.log('Creating new proxy contract...');
-      deployResult = await upgrades.deployProxy(Contract, ARGUMENTS);
+      deployResult = await upgrades.deployProxy(
+        Contract,
+        ARGUMENTS,
+        DEFAULT_PROXY_SETTINGS
+      );
       await deployResult.deployed();
       await proxyStore(CONTRACT_NAME, deployResult.address, hre.network.name);
       console.log(
@@ -45,9 +96,8 @@ const deploy = async () => {
       console.error('Could not write deploy files', e);
     }
   }
+
+  return deployResult;
 };
 
-console.log(':: Starting Deployment ::');
-console.log('Network:', NETWORK);
-console.log('Contract:', CONTRACT_NAME);
-deploy();
+main();
