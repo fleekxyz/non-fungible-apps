5994e30e8a641a26e61b2ea1bc36ea8a04026dbd
Camila Sosa Morales <camisosa@fleek.co>
2023-01-27 10:23:27 -0500

    chore: added input file on home view

--- ui/src/views/home/home.tsx
+++ ui/src/views/home/home.tsx
@@ -1,10 +1,22 @@
 import { Flex } from '@/components';
-import React from 'react';
+import { Form } from '@/components';
+import React, { useState } from 'react';
 
 export const Home = () => {
+  const [file, setFile] = useState<File | null>(null);
   return (
-    <Flex css={{ justifyContent: 'center' }}>
+    <Flex
+      css={{
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <h1>Home</h1>
+      <Form.Field css={{ width: '$24' }}>
+        <Form.Label>Label</Form.Label>
+        <Form.LogoFileInput value={file} onChange={(file) => setFile(file)} />
+      </Form.Field>
     </Flex>
   );
 };
