6dcf58fb7ecd88c26313ac3d910b8dabb3ce405a
Camila Sosa Morales <camisosa@fleek.co>
2023-01-27 10:09:25 -0500

    chore: stepper on home view

--- ui/src/views/home/home.tsx
+++ ui/src/views/home/home.tsx
@@ -1,10 +1,12 @@
 import { Flex } from '@/components';
+import { MintStepper } from '@/components/layout/stepper';
 import React from 'react';
 
 export const Home = () => {
   return (
     <Flex css={{ justifyContent: 'center' }}>
       <h1>Home</h1>
+      <MintStepper />
     </Flex>
   );
 };
