4e9023ce3fb0fb6322d53ad4847c2c858fa000e8
Camila Sosa Morales <camisosa@fleek.xyz>
2023-01-30 12:10:47 -0500

    chore: input component (#84)

--- ui/src/components/core/icon/icon-library.tsx
+++ ui/src/components/core/icon/icon-library.tsx
@@ -1,13 +1,17 @@
 import { IoLogoGithub } from '@react-icons/all-files/io5/IoLogoGithub';
 import { IoArrowBackCircleSharp } from '@react-icons/all-files/io5/IoArrowBackCircleSharp';
 import { IoInformationCircleSharp } from '@react-icons/all-files/io5/IoInformationCircleSharp';
+import { IoCloudUploadSharp } from '@react-icons/all-files/io5/IoCloudUploadSharp';
+import { AiOutlineCheck } from '@react-icons/all-files/ai/AiOutlineCheck';
 import { MetamaskIcon, EthereumIcon } from './custom';
 
 export const IconLibrary = Object.freeze({
   back: IoArrowBackCircleSharp,
+  check: AiOutlineCheck,
   ethereum: EthereumIcon,
   github: IoLogoGithub,
   info: IoInformationCircleSharp,
+  upload: IoCloudUploadSharp,
   metamask: MetamaskIcon, //remove if not used
 });
 
