76f9e0482f8fc4c98d616d4d7463cb7bcf837c26
Felipe Mendes <zo.fmendes@gmail.com>
2023-02-02 19:12:37 +0000

    chore: add FleekERC721 hardhat test helpers

---
+++ test/hardhat/contracts/FleekERC721/helpers/fixture.ts
@@ -0,0 +1,49 @@
+import { ethers, upgrades } from 'hardhat';
+import { TestConstants } from './constants';
+
+export abstract class Fixtures {
+  static async default() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const libraries = {
+      FleekSVG: (await (await ethers.getContractFactory('FleekSVG')).deploy())
+        .address,
+    };
+
+    const Contract = await ethers.getContractFactory('FleekERC721', {
+      libraries,
+    });
+    const contract = await upgrades.deployProxy(
+      Contract,
+      [
+        TestConstants.CollectionParams.name,
+        TestConstants.CollectionParams.symbol,
+      ],
+      {
+        unsafeAllow: ['external-library-linking'],
+      }
+    );
+
+    return { owner, otherAccount, contract };
+  }
+
+  static async withMint() {
+    const fromDefault = await this.default();
+
+    const response = await fromDefault.contract.mint(
+      fromDefault.owner.address,
+      TestConstants.MintParams.name,
+      TestConstants.MintParams.description,
+      TestConstants.MintParams.externalUrl,
+      TestConstants.MintParams.ens,
+      TestConstants.MintParams.commitHash,
+      TestConstants.MintParams.gitRepository,
+      TestConstants.MintParams.logo,
+      TestConstants.MintParams.color
+    );
+
+    const tokenId = response.value.toNumber();
+    return { ...fromDefault, tokenId };
+  }
+}
