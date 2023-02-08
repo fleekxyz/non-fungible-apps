0e1654263e6ebc5b5881e62e0390992e33c7d0e7
Camila Sosa Morales <camisosa@fleek.co>
2023-02-07 22:13:24 -0300

    chore: add card component

---
+++ ui/src/components/card/card.styles.ts
@@ -0,0 +1,35 @@
+import { dripStitches } from '@/theme';
+
+const { styled } = dripStitches;
+
+export abstract class CardStyles {
+  static readonly Container = styled('div', {
+    backgroundColor: '$slate2',
+    borderRadius: '$xlh',
+    padding: '$7',
+    borderStyle: 'solid',
+    borderColor: '$slate6',
+    borderWidth: '$default',
+  });
+
+  static readonly Heading = styled('h3', {
+    color: '$slate12',
+    fontSize: '$xl',
+    fontWeight: '$medium',
+  });
+
+  static readonly Body = styled('div', {
+    pt: '$5',
+  });
+
+  static readonly CardText = styled('div', {
+    backgroundColor: '$slate1',
+    borderRadius: '$xl',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    color: '$slate8',
+  });
+}
---
+++ ui/src/components/card/card.tsx
@@ -0,0 +1,69 @@
+import React, { forwardRef } from 'react';
+import { Flex } from '../layout';
+import { CardStyles } from './card.styles';
+
+export abstract class Card {
+  static readonly Container = forwardRef<HTMLDivElement, Card.ContainerProps>(
+    ({ children, ...props }, ref) => {
+      return (
+        <CardStyles.Container ref={ref} {...props}>
+          {children}
+        </CardStyles.Container>
+      );
+    }
+  );
+
+  static readonly Heading = forwardRef<HTMLHeadingElement, Card.HeadingProps>(
+    ({ title, leftIcon, rightIcon, ...props }, ref) => {
+      return (
+        <Flex css={{ justifyContent: 'space-between' }}>
+          <Flex>
+            {leftIcon}
+            <CardStyles.Heading ref={ref} {...props}>
+              {title}
+            </CardStyles.Heading>
+          </Flex>
+          {rightIcon}
+        </Flex>
+      );
+    }
+  );
+
+  static readonly Body = forwardRef<HTMLDivElement, Card.BodyProps>(
+    ({ children, ...props }, ref) => {
+      return (
+        <CardStyles.Body ref={ref} {...props}>
+          {children}
+        </CardStyles.Body>
+      );
+    }
+  );
+
+  static readonly CardText = forwardRef<HTMLDivElement, Card.CardTextProps>(
+    ({ children, ...props }, ref) => {
+      return (
+        <CardStyles.CardText {...props} ref={ref}>
+          {children}
+        </CardStyles.CardText>
+      );
+    }
+  );
+}
+
+export namespace Card {
+  export type ContainerProps = {
+    children: React.ReactNode;
+  } & React.ComponentProps<typeof CardStyles.Container>;
+
+  export type HeadingProps = {
+    title: string;
+    leftIcon?: React.ReactNode;
+    rightIcon?: React.ReactNode;
+  } & React.ComponentProps<typeof CardStyles.Heading>;
+
+  export type BodyProps = { children: React.ReactNode } & React.ComponentProps<
+    typeof CardStyles.Body
+  >;
+
+  export type CardTextProps = React.ComponentProps<typeof CardStyles.CardText>;
+}
