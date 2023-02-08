94fdf62e49feebb2107c50d6a0a6014215b59c65
Camila Sosa Morales <camisosa@fleek.co>
2023-02-08 10:33:31 -0300

    chore: apply PR review

--- ui/src/components/core/combobox/combobox.tsx
+++ ui/src/components/core/combobox/combobox.tsx
@@ -3,6 +3,74 @@ import { Combobox as ComboboxLib, Transition } from '@headlessui/react';
 import { Icon, IconName } from '@/components/core/icon';
 import { Flex } from '@/components/layout';
 
+type ComboboxInputProps = {
+  open: boolean;
+  handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleInputClick: () => void;
+};
+
+const ComboboxInput = ({
+  open,
+  handleInputChange,
+  handleInputClick,
+}: ComboboxInputProps) => (
+  <div className="relative">
+    <Icon
+      name="search"
+      size="sm"
+      css={{
+        position: 'absolute',
+        left: '$3',
+        top: '0.9375rem',
+        color: 'slate8',
+      }}
+    />
+    <ComboboxLib.Input
+      placeholder="Search"
+      className={`w-full border-solid border border-slate7  py-3 pl-8 pr-10 text-sm bg-transparent leading-5 text-slate11 outline-none ${
+        open ? 'border-b-0 rounded-t-xl bg-black border-slate6' : 'rounded-xl'
+      }`}
+      displayValue={(selectedValue: ComboboxItem) => selectedValue.label}
+      onChange={handleInputChange}
+      onClick={handleInputClick}
+    />
+  </div>
+);
+
+type ComboboxOptionProps = {
+  option: ComboboxItem;
+};
+
+const ComboboxOption = ({ option }: ComboboxOptionProps) => (
+  <ComboboxLib.Option
+    key={option.value}
+    value={option}
+    className={({ active }) =>
+      `relative cursor-default select-none py-2 px-3.5 text-slate11 rounded-xl mb-2 text-sm ${
+        active ? 'bg-slate5 text-slate12' : 'bg-transparent'
+      }`
+    }
+  >
+    {({ selected, active }) => (
+      <Flex css={{ justifyContent: 'space-between' }}>
+        <Flex css={{ flexDirection: 'row' }}>
+          {option.icon && <Icon name={option.icon} css={{ mr: '$2' }} />}
+          <span className={`${active ? 'text-slate12' : 'text-slate11'}`}>
+            {option.label}
+          </span>
+        </Flex>
+        {selected && <Icon name="check" color="white" />}
+      </Flex>
+    )}
+  </ComboboxLib.Option>
+);
+
+const NoResults = () => (
+  <div className="relative cursor-default select-none pt-2 px-3.5 pb-4 text-slate11">
+    Nothing found.
+  </div>
+);
+
 export type ComboboxItem = {
   value: string;
   label: string;
@@ -34,40 +102,36 @@ export const Combobox: React.FC<ComboboxProps> = ({
             .includes(query.toLowerCase().replace(/\s+/g, ''))
         );
 
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const handleInputClick = () => {
+    buttonRef.current?.click();
+  };
+
+  const handleComboboxChange = (option: ComboboxItem) => {
+    onChange(option);
+  };
+
+  const handleLeaveTransition = () => {
+    setQuery('');
+  };
+
   return (
     <ComboboxLib
       value={selectedValue}
       by="value"
-      onChange={(option: ComboboxItem) => onChange(option)}
+      onChange={handleComboboxChange}
     >
       {({ open }) => (
         <>
           <div className="w-full cursor-default overflow-hidden bg-transparent text-left focus:outline-none sm:text-sm">
-            <div className="relative">
-              <Icon
-                name="search"
-                size="sm"
-                css={{
-                  position: 'absolute',
-                  left: '$3',
-                  top: '0.9375rem',
-                  color: 'slate8',
-                }}
-              />
-              <ComboboxLib.Input
-                placeholder="Search"
-                className={`w-full border-solid border border-slate7  py-3 pl-8 pr-10 text-sm bg-transparent leading-5 text-slate11 outline-none ${
-                  open
-                    ? 'border-b-0 rounded-t-xl bg-black border-slate6'
-                    : 'rounded-xl'
-                }`}
-                displayValue={(selectedValue: ComboboxItem) =>
-                  selectedValue.label
-                }
-                onChange={(event) => setQuery(event.target.value)}
-                onClick={() => buttonRef.current?.click()}
-              />
-            </div>
+            <ComboboxInput
+              handleInputChange={handleInputChange}
+              handleInputClick={handleInputClick}
+              open={open}
+            />
             <ComboboxLib.Button ref={buttonRef} className="hidden" />
           </div>
           <Transition
@@ -77,44 +141,14 @@ export const Combobox: React.FC<ComboboxProps> = ({
             leave="transition ease-out duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
-            afterLeave={() => setQuery('')}
+            afterLeave={handleLeaveTransition}
           >
             <ComboboxLib.Options className="absolute max-h-60 w-inherit z-10 overflow-auto rounded-b-xl border-solid  border-slate6  border  bg-black pt-2 px-3 text-base focus:outline-none sm:text-sm">
               {filteredItems.length === 0 && query !== '' ? (
-                <div className="relative cursor-default select-none pt-2 px-3.5 pb-4 text-slate11">
-                  Nothing found.
-                </div>
+                <NoResults />
               ) : (
                 filteredItems.map((option) => {
-                  return (
-                    <ComboboxLib.Option
-                      key={option.value}
-                      value={option}
-                      className={({ active }) =>
-                        `relative cursor-default select-none py-2 px-3.5 text-slate11 rounded-xl mb-2 text-sm ${
-                          active ? 'bg-slate5 text-slate12' : 'bg-transparent'
-                        }`
-                      }
-                    >
-                      {({ selected, active }) => (
-                        <Flex css={{ justifyContent: 'space-between' }}>
-                          <Flex css={{ flexDirection: 'row' }}>
-                            {option.icon && (
-                              <Icon name={option.icon} css={{ mr: '$2' }} />
-                            )}
-                            <span
-                              className={`${
-                                active ? 'text-slate12' : 'text-slate11'
-                              }`}
-                            >
-                              {option.label}
-                            </span>
-                          </Flex>
-                          {selected && <Icon name="check" color="white" />}
-                        </Flex>
-                      )}
-                    </ComboboxLib.Option>
-                  );
+                  return <ComboboxOption option={option} />;
                 })
               )}
             </ComboboxLib.Options>
