68dda369ecc01921491deb4ec5501d35725fd0eb
EmperorOrokuSaki <artie.eth@gmail.com>
2023-02-09 20:21:00 +0330

    chore: update foundry tests to match the new interface of the contract.

--- contracts/test/hardhat/contracts/FleekERC721/access-point/access-points-autoapproval-on.t.ts
+++ contracts/test/hardhat/contracts/FleekERC721/access-point/access-points-autoapproval-on.t.ts
@@ -18,8 +18,6 @@ describe('AccessPoints with Auto Approval on', () => {
     await expect(contract.addAccessPoint(tokenId, 'accesspoint.com'))
       .to.emit(contract, 'NewAccessPoint')
       .withArgs('accesspoint.com', tokenId, owner.address);
-
-    expect(await contract.appAccessPoints(tokenId)).eql(['accesspoint.com']);
   });
 
   it('should return a AP json object', async () => {
@@ -109,7 +107,9 @@ describe('AccessPoints with Auto Approval on', () => {
       .to.emit(contract, 'RemoveAccessPoint')
       .withArgs('accesspoint.com', tokenId, owner.address);
 
-    expect(await contract.appAccessPoints(tokenId)).eql([]);
+    await expect(
+      contract.getAccessPointJSON('accesspoint.com')
+    ).to.be.revertedWith('FleekERC721: invalid AP');
   });
 
   it('should allow only AP owner to remove it', async () => {
@@ -192,42 +192,5 @@ describe('AccessPoints with Auto Approval on', () => {
     const parsedAp = JSON.parse(ap);
 
     expect(parsedAp.nameVerified).to.be.false;
-  });
-
-  it('should get a list of added APs for an app', async () => {
-    const { contract, tokenId } = fixture;
-
-    await contract.addAccessPoint(tokenId, 'accesspoint1.com');
-    await contract.addAccessPoint(tokenId, 'accesspoint2.com');
-    await contract.addAccessPoint(tokenId, 'accesspoint3.com');
-    await contract.addAccessPoint(tokenId, 'accesspoint4.com');
-
-    const aps = await contract.appAccessPoints(tokenId);
-
-    expect(aps).to.eql([
-      'accesspoint1.com',
-      'accesspoint2.com',
-      'accesspoint3.com',
-      'accesspoint4.com',
-    ]);
-  });
-
-  it('should get a list of added APs for an app after removing one', async () => {
-    const { contract, tokenId } = fixture;
-
-    await contract.addAccessPoint(tokenId, 'accesspoint1.com');
-    await contract.addAccessPoint(tokenId, 'accesspoint2.com');
-    await contract.addAccessPoint(tokenId, 'accesspoint3.com');
-    await contract.addAccessPoint(tokenId, 'accesspoint4.com');
-
-    await contract.removeAccessPoint('accesspoint2.com');
-
-    const aps = await contract.appAccessPoints(tokenId);
-
-    expect(aps).to.eql([
-      'accesspoint1.com',
-      'accesspoint4.com',
-      'accesspoint3.com',
-    ]);
   });
 });
