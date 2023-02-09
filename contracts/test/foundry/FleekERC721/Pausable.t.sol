8cfd3774c4b7b2d1f5412578975c742cd481a295
Felipe Mendes <zo.fmendes@gmail.com>
2023-02-09 21:36:16 +0000

    test: add test for functions when contract is paused

--- contracts/test/foundry/FleekERC721/Pausable.t.sol
+++ contracts/test/foundry/FleekERC721/Pausable.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.17;
 
 import "./TestBase.sol";
 import "contracts/FleekPausable.sol";
+import "contracts/FleekAccessControl.sol";
 
 contract Test_FleekERC721_PausableAssertions is Test {
     function expectRevertWithContractIsPaused() public {
@@ -83,5 +84,39 @@ contract Test_FleekERC721_Pausable is Test_FleekERC721_Base, Test_FleekERC721_Pa
 
         expectRevertWithPausableIsSetTo(false);
         CuT.setPausable(false);
+    }
+
+    function test_shouldRevertForFunctionsWhenContractIsPaused() public {
+        address randomAddress = address(1);
+        CuT.unpause();
+        uint256 tokenId = mintDefault(deployer);
+        CuT.pause();
+
+        expectRevertWithContractIsPaused();
+        mintDefault(deployer);
+
+        expectRevertWithContractIsPaused();
+        CuT.burn(tokenId);
+
+        expectRevertWithContractIsPaused();
+        CuT.transferFrom(deployer, randomAddress, tokenId);
+
+        expectRevertWithContractIsPaused();
+        CuT.addAccessPoint(tokenId, "accesspoint.com");
+
+        expectRevertWithContractIsPaused();
+        CuT.removeAccessPoint("accesspoint.com");
+
+        expectRevertWithContractIsPaused();
+        CuT.grantCollectionRole(FleekAccessControl.Roles.Controller, randomAddress);
+
+        expectRevertWithContractIsPaused();
+        CuT.revokeCollectionRole(FleekAccessControl.Roles.Controller, randomAddress);
+
+        expectRevertWithContractIsPaused();
+        CuT.grantTokenRole(tokenId, FleekAccessControl.Roles.Controller, randomAddress);
+
+        expectRevertWithContractIsPaused();
+        CuT.revokeTokenRole(tokenId, FleekAccessControl.Roles.Controller, randomAddress);
     }
 }
