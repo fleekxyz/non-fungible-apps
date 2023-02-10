e1370f42724a7ea6afb1ac52e984eb8ac7036366
EmperorOrokuSaki <artie.eth@gmail.com>
2023-02-10 14:41:19 +0330

    test: add test for getToken to hardhat

---
+++ contracts/test/hardhat/contracts/FleekERC721/get-token.t.ts
@@ -0,0 +1,28 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { TestConstants, Fixtures, parseTokenURI } from './helpers';
+import { ethers } from 'hardhat';
+const { MintParams } = TestConstants;
+
+describe('FleekERC721.GetToken', () => {
+  let fixture: Awaited<ReturnType<typeof Fixtures.withMint>>;
+
+  before(async () => {
+    fixture = await loadFixture(Fixtures.withMint);
+  });
+
+  it('should return the token metadata without nested mappings', async () => {
+    const { contract, tokenId } = fixture;
+    const metadata = await contract.getToken(tokenId);
+
+    expect(metadata).to.eql([
+      MintParams.name,
+      MintParams.description,
+      MintParams.externalUrl,
+      MintParams.ens,
+      ethers.BigNumber.from(0),
+      MintParams.logo,
+      MintParams.color,
+    ]);
+  });
+});
