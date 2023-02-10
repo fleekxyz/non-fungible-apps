c8169e82b8ce2368952079158e301da35659a686
Felipe Mendes <zo.fmendes@gmail.com>
2023-02-10 19:01:39 +0000

    test: fix hardhat tests for token roles

---
+++ contracts/test/hardhat/contracts/FleekERC721/helpers/error.ts
@@ -0,0 +1,7 @@
+export const Errors = Object.freeze({
+  MustHaveCollectionRole: 'MustHaveCollectionRole',
+  MustHaveTokenRole: 'MustHaveTokenRole',
+  MustHaveAtLeastOneOwner: 'MustHaveAtLeastOneOwner',
+  RoleAlreadySet: 'RoleAlreadySet',
+  MustBeTokenOwner: 'MustBeTokenOwner',
+});
--- contracts/test/hardhat/contracts/FleekERC721/token-roles.t.ts
+++ contracts/test/hardhat/contracts/FleekERC721/token-roles.t.ts
@@ -1,8 +1,8 @@
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
-import { TestConstants, Fixtures, parseTokenURI } from './helpers';
+import { TestConstants, Fixtures, parseTokenURI, Errors } from './helpers';
 
-const { Roles } = TestConstants;
+const { TokenRoles } = TestConstants;
 
 describe('FleekERC721.TokenRoles', () => {
   let fixture: Awaited<ReturnType<typeof Fixtures.withMint>>;
@@ -19,27 +19,23 @@ describe('FleekERC721.TokenRoles', () => {
 
   it('should match the owner role for minter', async () => {
     const { contract, owner, tokenId } = fixture;
-    const hasRole = await contract.hasTokenRole(
-      tokenId,
-      Roles.Owner,
-      owner.address
-    );
+    const tokenOwner = await contract.ownerOf(tokenId);
 
-    expect(hasRole).to.be.true;
+    expect(tokenOwner).to.be.equal(owner.address);
   });
 
   it('should add a new controller', async () => {
     const { contract, owner, otherAccount, tokenId } = fixture;
     await contract.grantTokenRole(
       tokenId,
-      Roles.Controller,
+      TokenRoles.Controller,
       otherAccount.address
     );
 
     expect(
       await contract.hasTokenRole(
         tokenId,
-        Roles.Controller,
+        TokenRoles.Controller,
         otherAccount.address
       )
     ).to.be.true;
@@ -49,71 +45,55 @@ describe('FleekERC721.TokenRoles', () => {
     const { contract, tokenId } = fixture;
     await contract.grantTokenRole(
       tokenId,
-      Roles.Controller,
+      TokenRoles.Controller,
       '0x7ED735b7095C05d78dF169F991f2b7f1A1F1A049'
     );
     await contract.grantTokenRole(
       tokenId,
-      Roles.Controller,
+      TokenRoles.Controller,
       '0x2FEd6Ef3c495922263B403319FA6DDB323DD49E3'
     );
 
     expect(
-      await contract.getTokenRoleMembers(tokenId, Roles.Controller)
-    ).to.eql([
-      '0x7ED735b7095C05d78dF169F991f2b7f1A1F1A049',
-      '0x2FEd6Ef3c495922263B403319FA6DDB323DD49E3',
-    ]);
-  });
-
-  it('should add a list of owners', async () => {
-    const { contract, owner, tokenId } = fixture;
-    await contract.grantTokenRole(
-      tokenId,
-      Roles.Owner,
-      '0x7ED735b7095C05d78dF169F991f2b7f1A1F1A049'
-    );
-    await contract.grantTokenRole(
-      tokenId,
-      Roles.Owner,
-      '0x2FEd6Ef3c495922263B403319FA6DDB323DD49E3'
-    );
-
-    expect(await contract.getTokenRoleMembers(tokenId, Roles.Owner)).to.eql([
-      owner.address,
-      '0x7ED735b7095C05d78dF169F991f2b7f1A1F1A049',
-      '0x2FEd6Ef3c495922263B403319FA6DDB323DD49E3',
-    ]);
+      await contract.hasTokenRole(
+        tokenId,
+        TokenRoles.Controller,
+        '0x7ED735b7095C05d78dF169F991f2b7f1A1F1A049'
+      )
+    ).to.be.true;
+    expect(
+      await contract.hasTokenRole(
+        tokenId,
+        TokenRoles.Controller,
+        '0x2FEd6Ef3c495922263B403319FA6DDB323DD49E3'
+      )
+    ).to.be.true;
   });
 
   it('should not match the owner role for other account', async () => {
     const { contract, otherAccount, tokenId } = fixture;
-    const hasRole = await contract.hasTokenRole(
-      tokenId,
-      Roles.Owner,
-      otherAccount.address
-    );
+    const tokenOwner = await contract.ownerOf(tokenId);
 
-    expect(hasRole).to.be.false;
+    expect(tokenOwner).to.not.be.equal(otherAccount.address);
   });
 
   it('should remove an added controller', async () => {
     const { contract, owner, otherAccount, tokenId } = fixture;
     await contract.grantTokenRole(
       tokenId,
-      Roles.Controller,
+      TokenRoles.Controller,
       otherAccount.address
     );
     await contract.revokeTokenRole(
       tokenId,
-      Roles.Controller,
+      TokenRoles.Controller,
       otherAccount.address
     );
 
     expect(
       await contract.hasTokenRole(
         tokenId,
-        Roles.Controller,
+        TokenRoles.Controller,
         otherAccount.address
       )
     ).to.be.false;
@@ -124,86 +104,83 @@ describe('FleekERC721.TokenRoles', () => {
     await contract.transferFrom(owner.address, otherAccount.address, tokenId);
 
     expect(await contract.ownerOf(tokenId)).to.equal(otherAccount.address);
-    expect(
-      await contract.hasTokenRole(tokenId, Roles.Owner, otherAccount.address)
-    ).to.be.true;
-    expect(await contract.hasTokenRole(tokenId, Roles.Owner, owner.address)).to
-      .be.false;
   });
 
   it('should clean the token controller list after transfer', async () => {
     const { contract, owner, otherAccount, tokenId } = fixture;
     await contract.grantTokenRole(
       tokenId,
-      Roles.Controller,
+      TokenRoles.Controller,
       otherAccount.address
     );
     await contract.transferFrom(owner.address, otherAccount.address, tokenId);
 
-    expect(await contract.getTokenRoleMembers(tokenId, 1)).to.eql([]);
+    expect(
+      await contract.hasTokenRole(
+        tokenId,
+        TokenRoles.Controller,
+        otherAccount.address
+      )
+    ).to.be.false;
   });
 
   it('should not be able to add address role', async () => {
-    const { contract, owner, otherAccount, tokenId } = fixture;
+    const { contract, otherAccount, tokenId } = fixture;
     await expect(
       contract
         .connect(otherAccount)
-        .grantTokenRole(tokenId, Roles.Owner, otherAccount.address)
-    ).to.be.revertedWith('FleekAccessControl: must have token role');
-
-    await expect(
-      contract
-        .connect(otherAccount)
-        .grantTokenRole(tokenId, Roles.Controller, otherAccount.address)
-    ).to.be.revertedWith('FleekAccessControl: must have token role');
+        .grantTokenRole(tokenId, TokenRoles.Controller, otherAccount.address)
+    ).to.be.revertedWithCustomError(contract, Errors.MustBeTokenOwner);
   });
 
   it('should not be able to remove address role', async () => {
     const { contract, owner, otherAccount, tokenId } = fixture;
-    await expect(
-      contract
-        .connect(otherAccount)
-        .revokeTokenRole(tokenId, Roles.Owner, otherAccount.address)
-    ).to.be.revertedWith('FleekAccessControl: must have token role');
 
     await expect(
       contract
         .connect(otherAccount)
-        .revokeTokenRole(tokenId, Roles.Controller, otherAccount.address)
-    ).to.be.revertedWith('FleekAccessControl: must have token role');
-  });
-
-  it('should be able to add token role after owner role granted', async () => {
-    const { contract, owner, otherAccount, tokenId } = fixture;
-    await contract.grantTokenRole(tokenId, Roles.Owner, otherAccount.address);
-
-    expect(
-      await contract
-        .connect(otherAccount)
-        .grantTokenRole(tokenId, Roles.Controller, otherAccount.address)
-    ).to.not.be.reverted;
+        .revokeTokenRole(tokenId, TokenRoles.Controller, otherAccount.address)
+    ).to.be.revertedWithCustomError(contract, Errors.MustBeTokenOwner);
   });
 
   it('should emit event when token role is granted', async () => {
     const { contract, owner, otherAccount, tokenId } = fixture;
     await expect(
-      contract.grantTokenRole(tokenId, Roles.Controller, otherAccount.address)
+      contract.grantTokenRole(
+        tokenId,
+        TokenRoles.Controller,
+        otherAccount.address
+      )
     )
       .to.emit(contract, 'TokenRoleGranted')
-      .withArgs(tokenId, Roles.Controller, otherAccount.address, owner.address);
+      .withArgs(
+        tokenId,
+        TokenRoles.Controller,
+        otherAccount.address,
+        owner.address
+      );
   });
 
   it('should emit event when token role is revoked', async () => {
     const { contract, owner, otherAccount, tokenId } = fixture;
     await contract.grantTokenRole(
       tokenId,
-      Roles.Controller,
+      TokenRoles.Controller,
       otherAccount.address
     );
     await expect(
-      contract.revokeTokenRole(tokenId, Roles.Controller, otherAccount.address)
+      contract.revokeTokenRole(
+        tokenId,
+        TokenRoles.Controller,
+        otherAccount.address
+      )
     )
       .to.emit(contract, 'TokenRoleRevoked')
-      .withArgs(tokenId, Roles.Controller, otherAccount.address, owner.address);
+      .withArgs(
+        tokenId,
+        TokenRoles.Controller,
+        otherAccount.address,
+        owner.address
+      );
   });
 });
