ddb85a72cb507c255ced4bc3189e7506051fa504
Felipe Mendes <zo.fmendes@gmail.com>
2023-02-15 13:57:19 +0000

    feat: add base contract for billing

---
+++ contracts/contracts/FleekBilling.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+error RequiredBillingValue(uint value);
+
+abstract contract FleekBilling is Initializable {
+    /**
+     * @dev Available billing values.
+     */
+    enum Billing {
+        Mint,
+        Transfer
+    }
+
+    /**
+     * @dev Emitted when the billing value is changed.
+     */
+    event ChangeBilling(Billing key, uint256 price);
+
+    /**
+     * @dev Mapping of billing values.
+     */
+    mapping(Billing => uint256) public _billings;
+
+    /**
+     * @dev Initializes the contract by setting default billing values.
+     */
+    function __FleekBilling_init() internal onlyInitializing {
+        _setBilling(Billing.Mint, 0);
+        _setBilling(Billing.Transfer, 0);
+    }
+
+    /**
+     * @dev Returns the billing value for a given key.
+     */
+    function getBilling(Billing key) public view returns (uint256) {
+        return _billings[key];
+    }
+
+    /**
+     * @dev Sets the billing value for a given key.
+     */
+    function _setBilling(Billing key, uint256 price) internal {
+        _billings[key] = price;
+        emit ChangeBilling(key, price);
+    }
+
+    /**
+     * @dev Internal function to require a billing value.
+     */
+    function _requireBilling(Billing key) internal {
+        uint256 requiredValue = _billings[key];
+        if (msg.value < _billings[key]) revert RequiredBillingValue(requiredValue);
+    }
+}
