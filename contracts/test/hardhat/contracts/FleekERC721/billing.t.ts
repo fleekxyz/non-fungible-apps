ae29f55b479b10491075f42d66461f713bbba984
Felipe Mendes <zo.fmendes@gmail.com>
2023-02-17 19:24:29 +0000

    test: add hardhat tests for billing

--- contracts/test/hardhat/contracts/FleekERC721/billing.t.ts
+++ contracts/test/hardhat/contracts/FleekERC721/billing.t.ts
@@ -3,12 +3,35 @@ import { ethers } from 'hardhat';
 import { expect } from 'chai';
 import { Fixtures, TestConstants } from './helpers';
 
-const { Billing } = TestConstants;
+const { Billing, MintParams } = TestConstants;
+
+const MintError = 'RequiredBillingValue';
 
 describe('FleekERC721.Billing', () => {
   let fixture: Awaited<ReturnType<typeof Fixtures.withMint>>;
   const mintPrice = ethers.utils.parseEther('1');
   const addAPPrice = ethers.utils.parseEther('1');
+
+  const mint = (value?: any) => {
+    const { contract, owner } = fixture;
+    return contract.mint(
+      owner.address,
+      MintParams.name,
+      MintParams.description,
+      MintParams.externalUrl,
+      MintParams.ens,
+      MintParams.commitHash,
+      MintParams.gitRepository,
+      MintParams.logo,
+      MintParams.color,
+      { value }
+    );
+  };
+
+  const addAP = (value?: any) => {
+    const { contract } = fixture;
+    return contract.addAccessPoint(0, 'random.com', { value });
+  };
 
   beforeEach(async () => {
     fixture = await loadFixture(Fixtures.withMint);
@@ -23,5 +46,45 @@ describe('FleekERC721.Billing', () => {
     expect(await contract.getBilling(Billing.AddAccessPoint)).to.equal(
       addAPPrice
     );
+  });
+
+  it('should allow mint with transfer', async () => {
+    const { contract, owner } = fixture;
+    await mint(mintPrice);
+    expect(await contract.ownerOf(0)).to.equal(owner.address);
+  });
+
+  it('should not allow mint with empty value', async () => {
+    const { contract } = fixture;
+    await expect(mint())
+      .to.be.revertedWithCustomError(contract, MintError)
+      .withArgs(mintPrice);
+  });
+
+  it('should not allow mint with different value', async () => {
+    const { contract } = fixture;
+    await expect(mint(ethers.utils.parseEther('2')))
+      .to.be.revertedWithCustomError(contract, MintError)
+      .withArgs(mintPrice);
+  });
+
+  it('should allow add access point with transfer', async () => {
+    const { contract } = fixture;
+    await addAP(addAPPrice);
+    expect(await contract.getAccessPointJSON('random.com')).to.exist;
+  });
+
+  it('should not allow add access point with empty value', async () => {
+    const { contract } = fixture;
+    await expect(addAP())
+      .to.be.revertedWithCustomError(contract, MintError)
+      .withArgs(addAPPrice);
+  });
+
+  it('should not allow add access point with different value', async () => {
+    const { contract } = fixture;
+    await expect(addAP(ethers.utils.parseEther('2')))
+      .to.be.revertedWithCustomError(contract, MintError)
+      .withArgs(addAPPrice);
   });
 });
