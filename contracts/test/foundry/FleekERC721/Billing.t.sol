f22399308ce4dc4cb2606c9eedef9e0384bb1c8e
Felipe Mendes <zo.fmendes@gmail.com>
2023-02-17 17:41:44 +0000

    test: add access point billing foundry tests

--- contracts/test/foundry/FleekERC721/Billing.t.sol
+++ contracts/test/foundry/FleekERC721/Billing.t.sol
@@ -16,18 +16,18 @@ contract Test_FleekERC721_BillingAssertions is Test {
 contract Test_FleekERC721_Billing is Test_FleekERC721_Base, Test_FleekERC721_BillingAssertions {
     using Strings for address;
     uint256 internal tokenId;
-    uint256 internal mintPrice = 1 ether;
-    uint256 internal transferPrice = 1 ether;
+    uint256 internal constant mintPrice = 1 ether;
+    uint256 internal constant addAPPrice = 1 ether;
 
     function setUp() public {
         baseSetUp();
         tokenId = mintDefault(deployer);
         CuT.setBilling(FleekBilling.Billing.Mint, mintPrice);
-        CuT.setBilling(FleekBilling.Billing.Transfer, transferPrice);
+        CuT.setBilling(FleekBilling.Billing.AddAccessPoint, addAPPrice);
     }
 
     function test_mint() public {
-        CuT.mint{value: 1 ether}(
+        CuT.mint{value: mintPrice}(
             deployer,
             TestConstants.APP_NAME,
             TestConstants.APP_DESCRIPTION,
@@ -56,5 +56,16 @@ contract Test_FleekERC721_Billing is Test_FleekERC721_Base, Test_FleekERC721_Bil
             TestConstants.LOGO_0,
             TestConstants.APP_COLOR
         );
+    }
+
+    function test_addAccessPoint() public {
+        CuT.addAccessPoint{value: addAPPrice}(tokenId, "accesspoint.com");
+    }
+
+    function testFuzz_cannotAddAccessPointWithWrongValue(uint256 value) public {
+        vm.assume(value != addAPPrice);
+        vm.deal(deployer, value);
+        expectRevertWithRequiredBillingValue(addAPPrice);
+        CuT.addAccessPoint{value: value}(tokenId, "accesspoint.com");
     }
 }
