3a61101d07240f074ee09e8322b2296440f97181
Camila Sosa Morales <camisosa@fleek.co>
2023-02-21 15:35:44 -0300

    ed

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app';
 import './styles.css';
-import { Providers } from './providers/providers';
+import { Providers } from './providers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
