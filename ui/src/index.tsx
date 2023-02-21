a00ca1d0a605357f6635dbe514bc9966d3a275f3
Camila Sosa Morales <camisosa@fleek.co>
2023-02-21 15:34:57 -0300

    feat: create providers folder to keep clean code

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app';
 import './styles.css';
-import { Providers } from './providers';
+import { Providers } from './providers/providers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
