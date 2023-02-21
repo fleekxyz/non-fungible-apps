e6b5d79c786b29cfb1712aca3f50b0dfa50f6810
EmperorOrokuSaki <artie.eth@gmail.com>
2023-02-21 23:00:58 +0330

    fix: revert back to tokenOwner for setAutoApproval functions.

--- contracts/contracts/FleekERC721.sol
+++ contracts/contracts/FleekERC721.sol
@@ -267,7 +267,7 @@ contract FleekERC721 is Initializable, ERC721Upgradeable, FleekAccessControl, Fl
     function setAccessPointAutoApproval(
         uint256 tokenId,
         bool _apAutoApproval
-    ) public virtual requireTokenRole(tokenId, TokenRoles.Controller) {
+    ) public virtual requireTokenOwner(tokenId) {
         _requireMinted(tokenId);
         _apps[tokenId].accessPointAutoApproval = _apAutoApproval;
         emit ChangeAccessPointAutoApproval(tokenId, _apAutoApproval, msg.sender);
@@ -458,7 +458,7 @@ contract FleekERC721 is Initializable, ERC721Upgradeable, FleekAccessControl, Fl
         uint256 tokenId,
         string memory apName,
         bool approved
-    ) public requireTokenRole(tokenId, TokenRoles.Controller) {
+    ) public requireTokenOwner(tokenId) {
         AccessPoint storage accessPoint = _accessPoints[apName];
         require(
             accessPoint.tokenId == tokenId,
