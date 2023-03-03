71d16077c391a814447c1da723e2af00e7b51382
Felipe Mendes <zo.fmendes@gmail.com>
2023-03-03 18:27:54 +0000

    feat: add fleekERC721 redux state and billing states

--- ui/src/store/store.ts
+++ ui/src/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import githubReducer from './features/github/github-slice';
+import fleekERC721Reducer from './features/fleek-erc721/fleek-erc721-slice';
 
 export const store = configureStore({
   reducer: {
+    fleekERC721: fleekERC721Reducer,
     github: githubReducer,
   },
   middleware: (getDefaultMiddleware) =>
