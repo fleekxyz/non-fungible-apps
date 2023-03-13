1eed510527c81130a259384814050209c822a4c6
Camila Sosa Morales <camisosa@fleek.xyz>
2023-03-13 11:20:53 -0500

    styles: fix styles combobox and dropdown (#167)

--- ui/src/components/core/combobox/combobox.tsx
+++ ui/src/components/core/combobox/combobox.tsx
@@ -45,8 +45,10 @@ const ComboboxInput = ({
     />
     <ComboboxLib.Input
       placeholder="Search"
-      className={`w-full border-solid border border-slate7 h-11  py-3 pl-10 pr-10 text-sm bg-transparent leading-5 text-slate11 outline-none ${
-        open ? 'border-b-0 rounded-t-xl bg-black border-slate6' : 'rounded-xl'
+      className={`w-full border-solid border border-slate7 h-11  py-3 pl-10 pr-10 text-sm leading-5 text-slate11 outline-none ${
+        open
+          ? 'border-b-0 rounded-t-xl bg-black border-slate6'
+          : 'rounded-xl bg-transparent'
       }`}
       displayValue={(selectedValue: ComboboxItem) => selectedValue.label}
       onChange={handleInputChange}
