a47b9ba8a78d74547bdfc2851f75082930b57222
Camila Sosa Morales <camisosa@fleek.co>
2023-03-28 16:32:05 -0300

    chore: add dropdown variation

--- ui/src/components/core/combobox/dropdown.tsx
+++ ui/src/components/core/combobox/dropdown.tsx
@@ -46,63 +46,132 @@ const DropdownOption = ({ option }: DropdownOptionProps) => (
 );
 
 type DropdownButtonProps = {
+  /**
+   * The selected value of the dropdown.
+   */
   selectedValue: DropdownItem | undefined;
+  /**
+   * If it's true, the list of options will be displayed
+   */
   open: boolean;
+  /**
+   * Background color of the dropdown. Should be on tailwind palette.
+   */
+  backgroundColor?: string;
+  /**
+   * Text color of the dropdown. Should be on tailwind palette.
+   */
+  textColor?: string;
 };
 
-const DropdownButton = ({ selectedValue, open }: DropdownButtonProps) => (
-  <Listbox.Button
-    className={`relative w-full cursor-default border-solid border border-slate7  py-3 pl-3.5 pr-10 h-11 text-left focus:outline-none sm:text-sm ${
-      open
-        ? 'border-b-0 rounded-t-xl bg-black border-slate6'
-        : 'rounded-xl bg-transparent'
-    }`}
-  >
-    <span
-      className={`block truncate ${
-        selectedValue && selectedValue.label ? 'text-slate12' : 'text-slate11'
-      } break-words`}
+const DropdownButton = ({
+  selectedValue,
+  open, //TODO maybe would be deprecated
+  backgroundColor,
+  textColor,
+}: DropdownButtonProps) => {
+  const textColorCss = textColor ? `text-${textColor}` : 'text-slate12';
+  const borderColor = backgroundColor
+    ? `border-${backgroundColor}`
+    : 'border-slate7';
+  const backgroundColorClass = backgroundColor
+    ? `bg-${backgroundColor}`
+    : 'bg-transparent';
+
+  return (
+    <Listbox.Button
+      className={`relative w-full cursor-default ${borderColor} border-solid border rounded-xl py-3 pl-3.5 pr-10 h-11 text-left focus:outline-none sm:text-sm 
+       ${backgroundColorClass} 
+       `}
     >
-      {selectedValue && selectedValue.label ? selectedValue.label : 'Select'}
-    </span>
-    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4">
-      <Icon name="chevron-down" />
-    </span>
-  </Listbox.Button>
-);
+      <span
+        className={`block truncate ${
+          selectedValue && selectedValue.label
+            ? `${textColorCss}`
+            : 'text-slate11'
+        } break-words`}
+      >
+        {selectedValue && selectedValue.label ? selectedValue.label : 'Select'}
+      </span>
+      <span
+        className={`pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4 ${textColorCss}`}
+      >
+        <Icon name="chevron-down" />
+      </span>
+    </Listbox.Button>
+  );
+};
 
 export type DropdownItem = {
+  /**
+   * The key of the item.
+   */
   value: string;
+  /**
+   * The label to display of the item.
+   */
   label: string;
 };
 
 export type DropdownProps = {
+  /**
+   * List of items to be displayed in the dropdown.
+   */
   items: DropdownItem[];
+  /**
+   * The selected value of the dropdown.
+   */
   selectedValue: DropdownItem | undefined;
+  /**
+   * Callback when the selected value changes.
+   */
   onChange(option: DropdownItem): void;
+  /**
+   * Background color of the dropdown. Should be on tailwind palette. https://tailwindcss.com/docs/background-color
+   */
+  backgroundColor?: string;
+  /**
+   * Text color of the dropdown. Should be on tailwind palette. https://tailwindcss.com/docs/text-color
+   */
+  textColor?: string;
+  /**
+   * Width of the options list. Should be on tailwind width. https://tailwindcss.com/docs/width
+   */
+  optionsWidth?: string;
 };
 
 export const Dropdown: React.FC<DropdownProps> = ({
   items,
   selectedValue,
   onChange,
+  backgroundColor,
+  textColor,
+  optionsWidth,
 }) => {
   const handleDropdownChange = (option: DropdownItem) => {
     onChange(option);
   };
+  const width = optionsWidth ? `w-${optionsWidth}` : 'w-full';
 
   return (
     <Listbox value={selectedValue} by="value" onChange={handleDropdownChange}>
       {({ open }) => (
         <div className="relative max-w-full">
-          <DropdownButton selectedValue={selectedValue} open={open} />
+          <DropdownButton
+            selectedValue={selectedValue}
+            open={open}
+            backgroundColor={backgroundColor}
+            textColor={textColor}
+          />
           <Transition
             as={Fragment}
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute max-h-32 w-full z-10 overflow-auto rounded-b-xl bg-black px-3 pt-2 border-solid  border-slate6  border text-base focus:outline-none sm:text-sm">
+            <Listbox.Options
+              className={`absolute mt-1 max-h-32 ${width} z-10 overflow-auto rounded-xl bg-black px-3 pt-2 border-solid  border-slate6  border text-base focus:outline-none sm:text-sm`}
+            >
               {items.map((option: DropdownItem) => (
                 <DropdownOption key={option.value} option={option} />
               ))}
--- ui/src/views/explore/list-nfas/results-search.tsx
+++ ui/src/views/explore/list-nfas/results-search.tsx
@@ -1,7 +1,16 @@
-import { Flex, Input } from '@/components';
+import { Dropdown, DropdownItem, Flex, Input } from '@/components';
+import { useState } from 'react';
 import { ResultsContainer, ResultsNumber, ResultsText } from './results.styles';
 
+const orderResults: DropdownItem[] = [
+  { value: 'a-z', label: 'Sort A-Z' },
+  { value: 'z-a', label: 'Sort Z-A' },
+];
+
 export const ResultsSearch: React.FC = () => {
+  const [selectedValue, setSelectedValue] = useState<DropdownItem>(
+    orderResults[0]
+  ); //TODO replace for context
   return (
     <Flex css={{ justifyContent: 'space-between' }}>
       <ResultsContainer>
@@ -9,6 +18,14 @@ export const ResultsSearch: React.FC = () => {
         <ResultsNumber>(3,271)</ResultsNumber>
       </ResultsContainer>
       <Input placeholder="Search" leftIcon="search" css={{ width: '23rem' }} />
+      <Dropdown
+        items={orderResults}
+        selectedValue={selectedValue}
+        onChange={setSelectedValue}
+        backgroundColor="slate4"
+        textColor="slate11"
+        optionsWidth="40"
+      />
     </Flex>
   );
 };
