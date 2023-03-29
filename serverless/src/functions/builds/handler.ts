11fb9d4079541a2ba788385147e95e33d9802e35
janison <jsonsivar@gmail.com>
2023-03-29 18:06:23 -0400

    chore: fix bug executing function

--- serverless/src/functions/builds/handler.ts
+++ serverless/src/functions/builds/handler.ts
@@ -1,29 +1,24 @@
-import { APIGatewayEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyResult } from 'aws-lambda';
 import { formatJSONResponse } from '@libs/apiGateway';
-import { middyfy } from '@libs/lambda';
+// import { middyfy } from '@libs/lambda';
 
 import { v4 } from 'uuid';
 
-export const submitBuildInfo = middyfy(
-  async (
-    event: APIGatewayEvent,
-    context: Context
-  ): Promise<APIGatewayProxyResult> => {
-    try {
-      const id = v4();
-      const buildInfo = {
-        event,
-        buildId: id,
-        createdAt: new Date().toISOString(),
-      };
-      return formatJSONResponse({
-        buildInfo,
-      });
-    } catch (e) {
-      return formatJSONResponse({
-        status: 500,
-        message: e,
-      });
-    }
+export const submitBuildInfo = async (): Promise<APIGatewayProxyResult> => {
+  try {
+    const id = v4();
+    const buildInfo = {
+      buildId: id,
+      createdAt: new Date().toISOString(),
+    };
+
+    return formatJSONResponse({
+      buildInfo,
+    });
+  } catch (e) {
+    return formatJSONResponse({
+      status: 500,
+      message: e,
+    });
   }
-);
+};
