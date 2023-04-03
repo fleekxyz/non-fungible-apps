7c8f8400e9592b2ecac7d0405c0599be676c2da7
Camila Sosa Morales <camisosa@fleek.co>
2023-04-03 19:14:08 -0300

    chore: remove unused ref

--- ui/src/components/core/combobox/combobox.tsx
+++ ui/src/components/core/combobox/combobox.tsx
@@ -156,141 +156,136 @@ export type ComboboxProps = {
   error?: boolean;
 };
 
-export const Combobox = forwardRef<HTMLInputElement, ComboboxProps>(
-  ({
-    items,
-    selectedValue = { value: '', label: '' },
-    withAutocomplete = false,
-    leftIcon = 'search',
-    onChange,
-    onBlur,
-    error = false,
-  }) => {
-    const [filteredItems, setFilteredItems] = useState<ComboboxItem[]>([]);
-    const [autocompleteItems, setAutocompleteItems] = useState<ComboboxItem[]>(
-      []
-    );
+export const Combobox: React.FC<ComboboxProps> = ({
+  items,
+  selectedValue = { value: '', label: '' },
+  withAutocomplete = false,
+  leftIcon = 'search',
+  onChange,
+  onBlur,
+  error = false,
+}) => {
+  const [filteredItems, setFilteredItems] = useState<ComboboxItem[]>([]);
+  const [autocompleteItems, setAutocompleteItems] = useState<ComboboxItem[]>(
+    []
+  );
 
-    useEffect(() => {
-      // If the selected value doesn't exist in the list of items, we add it
-      if (
-        items.filter((item) => item === selectedValue).length === 0 &&
-        selectedValue.value !== undefined &&
-        autocompleteItems.length === 0 &&
-        withAutocomplete
-      ) {
-        setAutocompleteItems([selectedValue]);
-      }
-    }, [autocompleteItems.length, items, selectedValue, withAutocomplete]);
+  useEffect(() => {
+    // If the selected value doesn't exist in the list of items, we add it
+    if (
+      items.filter((item) => item === selectedValue).length === 0 &&
+      selectedValue.value !== undefined &&
+      autocompleteItems.length === 0 &&
+      withAutocomplete
+    ) {
+      setAutocompleteItems([selectedValue]);
+    }
+  }, [autocompleteItems.length, items, selectedValue, withAutocomplete]);
 
-    useEffect(() => {
+  useEffect(() => {
+    setFilteredItems(items);
+  }, [items]);
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleSearch = useDebounce((searchValue: string) => {
+    if (searchValue === '') {
       setFilteredItems(items);
-    }, [items]);
 
-    const buttonRef = useRef<HTMLButtonElement>(null);
-
-    const handleSearch = useDebounce((searchValue: string) => {
-      if (searchValue === '') {
-        setFilteredItems(items);
-
-        if (withAutocomplete) {
-          setAutocompleteItems([]);
-          handleComboboxChange({} as ComboboxItem);
-        }
-      } else {
-        const filteredValues = items.filter((item) =>
-          cleanString(item.label).startsWith(cleanString(searchValue))
-        );
-
-        if (withAutocomplete && filteredValues.length === 0) {
-          // If the search value doesn't exist in the list of items, we add it
-          setAutocompleteItems([{ value: searchValue, label: searchValue }]);
-        }
-        setFilteredItems(filteredValues);
-      }
-    }, 200);
-
-    const handleInputChange = (
-      event: React.ChangeEvent<HTMLInputElement>
-    ): void => {
-      event.stopPropagation();
-      handleSearch(event.target.value);
-    };
-
-    const handleInputClick = (): void => {
-      buttonRef.current?.click();
-    };
-
-    const handleComboboxChange = (optionSelected: ComboboxItem): void => {
-      onChange(optionSelected);
-    };
-
-    const handleLeaveTransition = (): void => {
-      setFilteredItems(items);
-      if (selectedValue.value === undefined && withAutocomplete) {
+      if (withAutocomplete) {
         setAutocompleteItems([]);
         handleComboboxChange({} as ComboboxItem);
       }
-    };
+    } else {
+      const filteredValues = items.filter((item) =>
+        cleanString(item.label).startsWith(cleanString(searchValue))
+      );
 
-    return (
-      <ComboboxLib
-        value={selectedValue}
-        by="value"
-        onChange={handleComboboxChange}
-      >
-        {({ open }) => (
-          <div className="relative">
-            <ComboboxInput
-              onChange={handleInputChange}
-              onClick={handleInputClick}
-              open={open}
-              leftIcon={leftIcon}
-              onBlur={onBlur}
-              error={error}
-            />
-            <ComboboxLib.Button ref={buttonRef} className="hidden" />
+      if (withAutocomplete && filteredValues.length === 0) {
+        // If the search value doesn't exist in the list of items, we add it
+        setAutocompleteItems([{ value: searchValue, label: searchValue }]);
+      }
+      setFilteredItems(filteredValues);
+    }
+  }, 200);
 
-            <Transition
-              show={open}
-              as={Fragment}
-              enter="transition duration-400 ease-out"
-              leave="transition ease-out duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-              afterLeave={handleLeaveTransition}
-            >
-              <ComboboxLib.Options className="absolute max-h-60 w-full z-10 overflow-auto rounded-b-xl border-solid  border-slate6  border  bg-black pt-2 px-3 text-base focus:outline-none sm:text-sm">
-                {[...autocompleteItems, ...filteredItems].length === 0 ||
-                filteredItems === undefined ? (
-                  <NoResults />
-                ) : (
-                  <>
-                    {autocompleteItems.length > 0 && <span>Create new</span>}
-                    {autocompleteItems.map(
-                      (autocompleteOption: ComboboxItem) => (
-                        <ComboboxOption
-                          key={autocompleteOption.value}
-                          option={autocompleteOption}
-                        />
-                      )
-                    )}
-                    {autocompleteItems.length > 0 &&
-                      filteredItems.length > 0 && (
-                        <Separator css={{ mb: '$2' }} />
-                      )}
-                    {filteredItems.map((option: ComboboxItem) => (
-                      <ComboboxOption key={option.value} option={option} />
-                    ))}
-                  </>
-                )}
-              </ComboboxLib.Options>
-            </Transition>
-          </div>
-        )}
-      </ComboboxLib>
-    );
-  }
-);
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    event.stopPropagation();
+    handleSearch(event.target.value);
+  };
+
+  const handleInputClick = (): void => {
+    buttonRef.current?.click();
+  };
+
+  const handleComboboxChange = (optionSelected: ComboboxItem): void => {
+    onChange(optionSelected);
+  };
+
+  const handleLeaveTransition = (): void => {
+    setFilteredItems(items);
+    if (selectedValue.value === undefined && withAutocomplete) {
+      setAutocompleteItems([]);
+      handleComboboxChange({} as ComboboxItem);
+    }
+  };
+
+  return (
+    <ComboboxLib
+      value={selectedValue}
+      by="value"
+      onChange={handleComboboxChange}
+    >
+      {({ open }) => (
+        <div className="relative">
+          <ComboboxInput
+            onChange={handleInputChange}
+            onClick={handleInputClick}
+            open={open}
+            leftIcon={leftIcon}
+            onBlur={onBlur}
+            error={error}
+          />
+          <ComboboxLib.Button ref={buttonRef} className="hidden" />
+
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="transition duration-400 ease-out"
+            leave="transition ease-out duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={handleLeaveTransition}
+          >
+            <ComboboxLib.Options className="absolute max-h-60 w-full z-10 overflow-auto rounded-b-xl border-solid  border-slate6  border  bg-black pt-2 px-3 text-base focus:outline-none sm:text-sm">
+              {[...autocompleteItems, ...filteredItems].length === 0 ||
+              filteredItems === undefined ? (
+                <NoResults />
+              ) : (
+                <>
+                  {autocompleteItems.length > 0 && <span>Create new</span>}
+                  {autocompleteItems.map((autocompleteOption: ComboboxItem) => (
+                    <ComboboxOption
+                      key={autocompleteOption.value}
+                      option={autocompleteOption}
+                    />
+                  ))}
+                  {autocompleteItems.length > 0 && filteredItems.length > 0 && (
+                    <Separator css={{ mb: '$2' }} />
+                  )}
+                  {filteredItems.map((option: ComboboxItem) => (
+                    <ComboboxOption key={option.value} option={option} />
+                  ))}
+                </>
+              )}
+            </ComboboxLib.Options>
+          </Transition>
+        </div>
+      )}
+    </ComboboxLib>
+  );
+};
 
 Combobox.displayName = 'Combobox';
