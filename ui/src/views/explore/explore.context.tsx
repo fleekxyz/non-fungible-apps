09e50adefc3ec5b1d0fff2e0076a135847e4c437
Camila Sosa Morales <camisosa@fleek.xyz>
2023-04-11 17:22:34 -0300

    feat: UI search on nfas list (#211)
    * wip: created context for explore view
    
    * feat: add search to context to filter on query
    
    * feat: add search value to query
    
    * feat: add search by name, external URL and ens
    
    * feat: search by name
    
    * chore: remove comments
    
    * chore: rename files explore view (#217)
    
    * chore: rename files explore view
    
    * Update ui/src/views/explore/explore-list/results-search.tsx
    
    Co-authored-by: Felipe Mendes <zo.fmendes@gmail.com>
    
    * chore: rename nfa search fragment
    
    ---------
    
    Co-authored-by: Felipe Mendes <zo.fmendes@gmail.com>
    
    * chore: update setPageNumber
    
    ---------
    
    Co-authored-by: Felipe Mendes <zo.fmendes@gmail.com>

---
+++ ui/src/views/explore/explore.context.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+import { OrderDirection } from '@/../.graphclient';
+import { createContext } from '@/utils';
+
+export type ExploreContext = {
+  search: string;
+  orderBy: string;
+  orderDirection: OrderDirection;
+  pageNumber: number;
+  endReached: boolean;
+  setSearch: (search: string) => void;
+  setOrderBy: (orderBy: string) => void;
+  setOrderDirection: (orderDirection: OrderDirection) => void;
+  setPageNumber: (pageNumber: number) => void;
+  setEndReached: (isEndReaced: boolean) => void;
+};
+
+const [ExploreProvider, useContext] = createContext<ExploreContext>({
+  name: 'Explore.Context',
+  hookName: 'Explore.useContext',
+  providerName: 'Explore.Provider',
+});
+
+export abstract class Explore {
+  static readonly useContext = useContext;
+
+  static readonly Provider: React.FC<Explore.ProviderProps> = ({
+    children,
+  }: Explore.ProviderProps) => {
+    const [search, setSearch] = useState('');
+    const [orderBy, setOrderBy] = useState('');
+    const [orderDirection, setOrderDirection] =
+      useState<OrderDirection>('desc');
+    const [pageNumber, setPageNumber] = useState(0);
+    const [endReached, setEndReached] = useState(false);
+
+    const context = {
+      search,
+      orderBy,
+      orderDirection,
+      pageNumber,
+      endReached,
+      setSearch,
+      setOrderBy,
+      setOrderDirection,
+      setPageNumber,
+      setEndReached,
+    };
+
+    return <ExploreProvider value={context}>{children}</ExploreProvider>;
+  };
+}
+
+export namespace Explore {
+  export type ProviderProps = {
+    children: React.ReactNode;
+  };
+}
