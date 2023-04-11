9bee4bc6020d20617712136d43e53d26f4d8d70d
Camila Sosa Morales <camisosa@fleek.co>
2023-04-11 15:34:15 -0300

    chore: rename nfa search fragment

--- ui/src/views/explore/explore-list/explore-list-container.tsx
+++ ui/src/views/explore/explore-list/explore-list-container.tsx
@@ -2,13 +2,13 @@ import { Flex } from '@/components';
 
 import { Explore } from '../explore.context';
 import { NFAListFragment } from './nfa-list';
-import { ResultsSearchFragment } from './results-search';
+import { NFASearchFragment } from './nfa-search';
 
 export const ExploreListContainer: React.FC = () => {
   return (
     <Flex css={{ flexDirection: 'column' }}>
       <Explore.Provider>
-        <ResultsSearchFragment />
+        <NFASearchFragment />
         <NFAListFragment />
       </Explore.Provider>
     </Flex>
