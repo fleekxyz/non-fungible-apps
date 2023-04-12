9113fdcca5af512581a2956ff945ae1a2b695c24
EmperorOrokuSaki <artie.eth@gmail.com>
2023-04-12 13:35:11 +0330

    feat: add log decoder helper and import web3.

---
+++ serverless/src/libs/log-decoder.ts
@@ -0,0 +1,8 @@
+var Web3 = require('web3');
+
+var web3 = new Web3(Web3.givenProvider || "ws://localhost:17895");
+
+export const logDecoder = (eventFieldsABI: { indexed: boolean; internalType: string; name: string; type: string; }[], data: string, topics: string[]) => {
+    return web3.eth.abi.decodeLog(eventFieldsABI, data, topics);
+};
+  
