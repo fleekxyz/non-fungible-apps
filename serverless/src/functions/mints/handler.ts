5d7764fc4e43f8dd6880495db3ab465357b91c22
EmperorOrokuSaki <artie.eth@gmail.com>
2023-04-12 12:29:15 +0330

    feat: add handler.

---
+++ serverless/src/functions/mints/handler.ts
@@ -0,0 +1,41 @@
+import {
+    APIGatewayProxyResult,
+    APIGatewayEvent,
+    APIGatewayEventRequestContext,
+  } from 'aws-lambda';
+  import { formatJSONResponse } from '@libs/api-gateway';
+  
+  import { v4 } from 'uuid';
+  
+  export const submitMintInfo = async (
+    event: APIGatewayEvent,
+    context: APIGatewayEventRequestContext
+  ): Promise<APIGatewayProxyResult> => {
+    try {
+      const id = v4();
+  
+      if (!verifyAlchemySig(event.headers.xalchemywork)) {
+        throw new Error('Invalid sig');
+      }
+  
+      const mintInfo = {
+        buildId: id,
+        createdAt: new Date().toISOString(),
+        tokenId: event.body.event.data,
+      };
+  
+      // check if we have it in mongo
+      // if so, trigger verification call
+      // if not, add to mongo
+  
+      return formatJSONResponse({
+        buildInfo,
+      });
+    } catch (e) {
+      return formatJSONResponse({
+        status: 500,
+        message: e,
+      });
+    }
+  };
+  
