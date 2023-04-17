589fb25ff05159fe3fd695e8f50431ad1b04b83e
Felipe Mendes <zo.fmendes@gmail.com>
2023-04-17 10:47:02 -0300

    feat: add resolved address component

--- ui/src/views/components-test/components-test.tsx
+++ ui/src/views/components-test/components-test.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@/components';
+import { Flex, ResolvedAddress } from '@/components';
 
 import { ColorPickerTest } from './color-picker';
 import { ComboboxTest } from './combobox-test';
@@ -7,6 +7,9 @@ import { ToastTest } from './toast-test';
 export const ComponentsTest: React.FC = () => {
   return (
     <Flex css={{ flexDirection: 'column' }}>
+      <ResolvedAddress css={{ alignSelf: 'center' }}>
+        {'0x7ed735b7095c05d78df169f991f2b7f1a1f1a049'}
+      </ResolvedAddress>
       <ComboboxTest />
       <ColorPickerTest />
       <ToastTest />
