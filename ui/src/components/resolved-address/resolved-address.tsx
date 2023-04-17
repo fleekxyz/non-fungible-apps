5ae09105c090869aef3e33f83c8b0244f644cc8d
Felipe Mendes <zo.fmendes@gmail.com>
2023-04-17 14:23:17 -0300

    feat: UI create functionality to resolve address (#227)
    * feat: add ENS slice
    
    * feat: add resolved address hook
    
    * feat: add resolved address component

---
+++ ui/src/components/resolved-address/resolved-address.tsx
@@ -0,0 +1,31 @@
+import { useMemo } from 'react';
+
+import { useResolvedAddress } from '@/store';
+import { forwardStyledRef } from '@/theme';
+
+import { ResolvedAddressStyles as RAS } from './resolved-address.styles';
+
+export type ResolvedAddressProps = React.ComponentPropsWithRef<
+  typeof RAS.Container
+> & {
+  children: string;
+};
+
+export const ResolvedAddress = forwardStyledRef<
+  HTMLSpanElement,
+  ResolvedAddressProps
+>(({ children, ...props }, ref) => {
+  const [resolvedAddress, loading] = useResolvedAddress(children);
+
+  const text = useMemo(() => {
+    if (!resolvedAddress.endsWith('.eth'))
+      return `${resolvedAddress.slice(0, 6)}...${resolvedAddress.slice(-4)}`;
+    return resolvedAddress;
+  }, [resolvedAddress]);
+
+  return (
+    <RAS.Container {...props} ref={ref} data-loading={loading}>
+      {text}
+    </RAS.Container>
+  );
+});
