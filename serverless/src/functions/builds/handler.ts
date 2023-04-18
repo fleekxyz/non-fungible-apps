be09a82d9e230eb0deda7935034e6b3048b80737
EmperorOrokuSaki <artie.eth@gmail.com>
2023-04-18 19:50:22 +0330

    feat: define interface.

--- serverless/src/functions/builds/handler.ts
+++ serverless/src/functions/builds/handler.ts
@@ -1,14 +1,17 @@
-import { APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 import { formatJSONResponse } from '@libs/api-gateway';
+const querystring = require('querystring');
 
 import { v4 } from 'uuid';
 
-export const submitBuildInfo = async (): Promise<APIGatewayProxyResult> => {
+export const submitBuildInfo = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   try {
+    const eventData = querystring.parse(event.body);
     const id = v4();
     const buildInfo = {
       buildId: id,
       createdAt: new Date().toISOString(),
+      submittedData: eventData
     };
 
     return formatJSONResponse({
