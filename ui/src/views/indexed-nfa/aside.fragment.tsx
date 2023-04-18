f8aa2e0fdb303f3a7b6b5cfa62427f7c36c68ec0
Felipe Mendes <zo.fmendes@gmail.com>
2023-04-18 14:10:40 -0300

    feat: indexed nfa aside styling

--- ui/src/views/indexed-nfa/aside.fragment.tsx
+++ ui/src/views/indexed-nfa/aside.fragment.tsx
@@ -1,6 +1,54 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 
+import { Button, Flex, Icon, NFAPreview } from '@/components';
+
+import { IndexedNFA } from './indexed-nfa.context';
 import { IndexedNFAStyles as S } from './indexed-nfa.styles';
+
+const Preview: React.FC = () => {
+  const { nfa } = IndexedNFA.useContext();
+
+  const color = useMemo(() => `#${nfa.color.toString(16)}`, [nfa]);
+
+  return (
+    <NFAPreview
+      color={color}
+      logo={nfa.logo}
+      ens={nfa.ENS}
+      name={nfa.name}
+      size="100%"
+      css={{
+        borderRadius: '$lg',
+        border: '1px solid $slate6',
+      }}
+    />
+  );
+};
+
+const CreateAccessPoint: React.FC = () => {
+  const { nfa } = IndexedNFA.useContext();
+  return (
+    <S.Aside.CreateAccessPoint.Container>
+      <S.Aside.CreateAccessPoint.Heading>
+        Host NFA Frontend
+      </S.Aside.CreateAccessPoint.Heading>
+      <S.Aside.CreateAccessPoint.Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae
+        ante erat. Sed quis finibus diam.
+      </S.Aside.CreateAccessPoint.Text>
+      <Flex css={{ gap: '$3' }}>
+        <Button as={Link} to={`create-ap/${nfa.tokenId}`} colorScheme="blue">
+          Host NFA Frontend
+        </Button>
+        <S.Aside.CreateAccessPoint.Extra href="#">
+          Learn more
+          <Icon name="chevron-down" />
+        </S.Aside.CreateAccessPoint.Extra>
+      </Flex>
+    </S.Aside.CreateAccessPoint.Container>
+  );
+};
 
 export const IndexedNFAAsideFragment: React.FC = () => {
   const ref = useRef<HTMLDivElement>(null);
@@ -11,5 +59,10 @@ export const IndexedNFAAsideFragment: React.FC = () => {
   }, [ref]);
 
   // TODO: improve fixed position on scroll
-  return <S.Aside ref={ref} css={{ top }} />;
+  return (
+    <S.Aside.Container ref={ref} css={{ top }}>
+      <Preview />
+      <CreateAccessPoint />
+    </S.Aside.Container>
+  );
 };
--- ui/src/views/indexed-nfa/indexed-nfa.styles.ts
+++ ui/src/views/indexed-nfa/indexed-nfa.styles.ts
@@ -5,17 +5,53 @@ export const IndexedNFAStyles = {
     position: 'relative',
     display: 'grid',
     gridTemplateAreas: '"aside main"',
+    gridTemplateColumns: '24.0625rem 1fr',
+    gridTemplateRows: 'fit-content',
+    gap: '$6',
   }),
-  Aside: styled('div', {
-    position: 'sticky',
-    top: 0,
-    gridArea: 'aside',
-    backgroundColor: 'red',
-    height: '20rem',
-  }),
+
+  Aside: {
+    Container: styled('aside', {
+      gridArea: 'aside',
+      position: 'sticky',
+
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '$5',
+      height: 'fit-content',
+    }),
+
+    CreateAccessPoint: {
+      Container: styled('div', {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '$5',
+        padding: '$5',
+        backgroundColor: '$blue1',
+        borderRadius: '$lg',
+      }),
+      Heading: styled('h2', {
+        fontSize: '$md',
+        color: '$slate12',
+      }),
+      Text: styled('p', {
+        fontSize: '$sm',
+        color: '$slate11',
+      }),
+      Extra: styled('a', {
+        display: 'flex',
+        alignItems: 'center',
+        color: '$slate11',
+        fontSize: '$sm',
+        gap: '$2',
+      }),
+    },
+  },
+
   Main: styled('main', {
     gridArea: 'main',
     backgroundColor: 'blue',
+    display: 'flex',
     height: '200vh',
   }),
 };
