787e4d6fef5ab4f447dbd51e08fbffae553679a1
Shredder <110225819+EmperorOrokuSaki@users.noreply.github.com>
2023-04-27 13:47:35 +0330

    feat: create smart contract helper for serverless (#232)
    * feat: add log decoder helper and import web3.
    
    * feat: create the web3 helper and install the package. update ts config to use JSONs in the helper script.
    
    * feat: use nfaContract in submitBuildInfo handler.
    
    * refactor: make the setTokenBuild params self-explanatory.
    
    * docs: comment the placeholder.

--- serverless/src/functions/builds/handler.ts
+++ serverless/src/functions/builds/handler.ts
@@ -2,6 +2,7 @@ import { APIGatewayProxyResult } from 'aws-lambda';
 import { formatJSONResponse } from '@libs/api-gateway';
 
 import { v4 } from 'uuid';
+import { nfaContract } from '@libs/nfa-contract';
 
 export const submitBuildInfo = async (): Promise<APIGatewayProxyResult> => {
   try {
@@ -10,6 +11,15 @@ export const submitBuildInfo = async (): Promise<APIGatewayProxyResult> => {
       buildId: id,
       createdAt: new Date().toISOString(),
     };
+
+    // place holder call
+    nfaContract.methods
+      .setTokenBuild(1, 'hash', 'repo')
+      .call((err: string | undefined, res: any) => {
+        if (err) throw new Error(err);
+        console.log('result');
+        console.log(res);
+      });
 
     return formatJSONResponse({
       buildInfo,
