bc27955e4099edc6abd7b77800c345701f677c48
EmperorOrokuSaki <artie.eth@gmail.com>
2023-04-27 18:41:48 +0330

    integration

--- serverless/src/functions/mints/handler.ts
+++ serverless/src/functions/mints/handler.ts
@@ -6,31 +6,148 @@ import {
 import { formatJSONResponse } from '@libs/api-gateway';
 
 import { v4 } from 'uuid';
+import { initPrisma, prisma } from '@libs/query-prisma';
+import { web3 } from '@libs/nfa-contract';
 
 export const submitMintInfo = async (
   event: APIGatewayEvent
   ///context: APIGatewayEventRequestContext
 ): Promise<APIGatewayProxyResult> => {
   try {
+    if (event.body === null) {
+      return formatJSONResponse({
+        status: 422,
+        message: 'Required parameters were not passed.',
+      });
+    }
     const id = v4();
 
     /**if (!verifyAlchemySig(event.headers.xalchemywork)) {
         throw new Error('Invalid sig');
       }**/
+    let topics = [
+      JSON.parse(event.body).event.data.block.logs[1].topics[1],
+      JSON.parse(event.body).event.data.block.logs[1].topics[2],
+      JSON.parse(event.body).event.data.block.logs[1].topics[3],
+    ];
+    const hexCalldata = JSON.parse(event.body).event.data.block.logs[1].data;
+    console.log(hexCalldata);
 
-    if (event.body == undefined) {
-      throw new Error('Undefined data');
-    }
+    const decodedLogs = web3.eth.abi.decodeLog(
+      [
+        {
+          indexed: true,
+          internalType: 'uint256',
+          name: 'tokenId',
+          type: 'uint256',
+        },
+        {
+          indexed: false,
+          internalType: 'string',
+          name: 'name',
+          type: 'string',
+        },
+        {
+          indexed: false,
+          internalType: 'string',
+          name: 'description',
+          type: 'string',
+        },
+        {
+          indexed: false,
+          internalType: 'string',
+          name: 'externalURL',
+          type: 'string',
+        },
+        {
+          indexed: false,
+          internalType: 'string',
+          name: 'ENS',
+          type: 'string',
+        },
+        {
+          indexed: false,
+          internalType: 'string',
+          name: 'commitHash',
+          type: 'string',
+        },
+        {
+          indexed: false,
+          internalType: 'string',
+          name: 'gitRepository',
+          type: 'string',
+        },
+        {
+          indexed: false,
+          internalType: 'string',
+          name: 'logo',
+          type: 'string',
+        },
+        {
+          indexed: false,
+          internalType: 'uint24',
+          name: 'color',
+          type: 'uint24',
+        },
+        {
+          indexed: false,
+          internalType: 'bool',
+          name: 'accessPointAutoApproval',
+          type: 'bool',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'minter',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'owner',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'verifier',
+          type: 'address',
+        },
+      ],
+      hexCalldata,
+      topics
+    );
 
     const mintInfo = {
       buildId: id,
       createdAt: new Date().toISOString(),
-      body: JSON.parse(event.body),
+      tokenId: decodedLogs.tokenId,
+      github_url: decodedLogs.githubRepository,
+      commit_hash: decodedLogs.commitHash,
+      owner: decodedLogs.owner,
     };
 
-    // check if we have it in mongo
-    // if so, trigger verification call
-    // if not, add to mongo
+    console.log(mintInfo);
+
+    initPrisma();
+
+    const token = await prisma.tokens.findRaw({
+      filter: {
+        tokenId: mintInfo.tokenId,
+      },
+    });
+
+    if (token.length == 0) {
+      // Add the token to the database
+      // await prisma.tokens.create({
+      //   data: {
+      //     tokenId: Number(mintInfo.tokenId),
+      //     github_url: mintInfo.github_url,
+      //     commit_hash: mintInfo.commit_hash,
+      //     owner: mintInfo.owner,
+      //   },
+      // });
+    }
 
     return formatJSONResponse({
       mintInfo,
--- serverless/src/libs/nfa-contract.ts
+++ serverless/src/libs/nfa-contract.ts
@@ -2,8 +2,8 @@ import Web3 from 'web3';
 import * as abiFile from '../../../contracts/deployments/goerli/FleekERC721.json';
 
 const contract_address = abiFile.address;
-const abi = abiFile.abi as any;
+export const abi = abiFile.abi as any;
 
-const web3 = new Web3('https://rpc.goerli.mudit.blog');
+export const web3 = new Web3('https://rpc.goerli.mudit.blog');
 
 export const nfaContract = new web3.eth.Contract(abi, contract_address);
