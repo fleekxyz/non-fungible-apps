9162a1f92be5f77edadfa2cc46abe16284fe5423
Camila Sosa Morales <camisosa@fleek.xyz>
2023-05-18 16:39:57 -0300

    fix: fix wallet button (#263)
    * fix: refactor connect button wallet to fix warning
    
    * chore: remove unused component

--- ui/src/views/mint/wallet-step/connect-wallet-button.tsx
+++ ui/src/views/mint/wallet-step/connect-wallet-button.tsx
@@ -1,26 +1,29 @@
 import { ConnectKitButton } from 'connectkit';
+import { useEffect } from 'react';
+import { useAccount } from 'wagmi';
 
 import { Stepper } from '@/components';
 
 import { ButtonConnection } from '../button-connection';
 
 export const ConnectWalletButton: React.FC = () => {
+  const { address } = useAccount();
   const { nextStep } = Stepper.useContext();
+
+  useEffect(() => {
+    if (address) nextStep();
+  }, [address, nextStep]);
 
   return (
     <ConnectKitButton.Custom>
-      {({ isConnected, show, address }) => {
-        if (isConnected && address) {
-          nextStep();
-        } else {
-          return (
-            <ButtonConnection
-              icon={'ethereum'}
-              label={'Connect Wallet'}
-              onClick={show}
-            />
-          );
-        }
+      {({ show }) => {
+        return (
+          <ButtonConnection
+            icon={'ethereum'}
+            label={'Connect Wallet'}
+            onClick={show}
+          />
+        );
       }}
     </ConnectKitButton.Custom>
   );
