a8ddfdfbb9294dfe437e1ee169fa1b004ad39c31
Camila Sosa Morales <camisosa@fleek.xyz>
2023-05-18 16:40:17 -0300

    Update ui/src/views/mint/nfa-step/verify-step/verify-nfa-step.styles.ts
    Co-authored-by: Felipe Mendes <zo.fmendes@gmail.com>

--- ui/src/views/mint/nfa-step/verify-step/verify-nfa-step.styles.ts
+++ ui/src/views/mint/nfa-step/verify-step/verify-nfa-step.styles.ts
@@ -10,7 +10,7 @@ export const VerifyNfaStepStyles = {
     Text: styled(Text, {
       color: '$slate11',
       fontSize: '$sm',
-      lineHeight: '20px',
+      lineHeight: '1.25rem',
     }),
     VerifyContainer: styled(Card.Text, {
       p: '$4',
