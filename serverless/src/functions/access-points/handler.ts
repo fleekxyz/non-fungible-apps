64d1ff02b685208905342966cd1ba22b837eea83
EmperorOrokuSaki <artie.eth@gmail.com>
2023-06-02 13:06:24 +0330

    feat: add call to the bunny cdn api.

---
+++ serverless/src/functions/access-points/handler.ts
@@ -0,0 +1,40 @@
+import { APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { formatJSONResponse } from '@libs/api-gateway';
+import * as dotenv from 'dotenv';
+import { BunnyCdn, LoadFreeCertificateMethodArgs } from '@libs/bunnyCDN';
+
+export const verifyAccessPoint = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  try {
+    // Check the parameters and environment variables
+    dotenv.config();
+    if (event.body === null || process.env.BUNNY_CDN_ACCESS_KEY == undefined) {
+      return formatJSONResponse({
+        status: 422,
+        message: 'Required parameters were not passed.',
+      });
+    }
+
+    // Set up constants
+    const bunnyCdn = new BunnyCdn(process.env.BUNNY_CDN_ACCESS_KEY);
+    const hostname = JSON.parse(event.body).hostname;
+
+    let args: LoadFreeCertificateMethodArgs = {
+      hostname,
+    };
+
+    await bunnyCdn.loadFreeCertificate(args).catch((e) => {
+      throw e;
+    });
+
+    return formatJSONResponse({
+      status: true,
+    });
+  } catch (e) {
+    return formatJSONResponse({
+      status: 500,
+      message: e,
+    });
+  }
+};
