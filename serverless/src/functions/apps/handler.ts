94313c030030c953f8fb938c24277e230f7beda2
Shredder <110225819+EmperorOrokuSaki@users.noreply.github.com>
2023-06-09 22:19:10 +0330

    feat: verify hostname (load free certificate call to bunny cdn) (#271)
    * feat: initialize the handler and function.
    
    * feat: add call to the bunny cdn api.
    
    * refactor: rename access points to apps.
    
    * refactor: remove redundant catch and throw
    
    Co-authored-by: Felipe Mendes <zo.fmendes@gmail.com>
    
    ---------
    
    Co-authored-by: Felipe Mendes <zo.fmendes@gmail.com>

--- serverless/src/functions/apps/handler.ts
+++ serverless/src/functions/apps/handler.ts
@@ -7,7 +7,42 @@ import {
   BunnyCdn,
   BunnyCdnError,
   CreatePullZoneMethodArgs,
+  LoadFreeCertificateMethodArgs,
 } from '@libs/bunnyCDN';
+
+export const verifyApp = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  try {
+    // Check the parameters and environment variables
+    dotenv.config();
+    if (event.body === null || process.env.BUNNY_CDN_ACCESS_KEY == undefined) {
+      return formatJSONResponse({
+        status: 422,
+        message: 'Required parameters were not passed.',
+      });
+    }
+
+    // Set up constants
+    const bunnyCdn = new BunnyCdn(process.env.BUNNY_CDN_ACCESS_KEY);
+    const hostname = JSON.parse(event.body).hostname;
+
+    let args: LoadFreeCertificateMethodArgs = {
+      hostname,
+    };
+
+    await bunnyCdn.loadFreeCertificate(args);
+
+    return formatJSONResponse({
+      status: true,
+    });
+  } catch (e) {
+    return formatJSONResponse({
+      status: 500,
+      message: e,
+    });
+  }
+};
 
 export const submitAppInfo = async (
   event: APIGatewayEvent
--- serverless/src/functions/apps/index.ts
+++ serverless/src/functions/apps/index.ts
@@ -1,5 +1,17 @@
 import { handlerPath } from '@libs/handler-resolver';
 
+export const verifyApp = {
+  handler: `${handlerPath(__dirname)}/handler.verifyApp`,
+  events: [
+    {
+      http: {
+        method: 'post',
+        path: 'verifyApp',
+      },
+    },
+  ],
+};
+
 export const submitAppInfo = {
   handler: `${handlerPath(__dirname)}/handler.submitAppInfo`,
   events: [
