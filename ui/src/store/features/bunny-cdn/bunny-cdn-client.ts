0bd6d070153d5e2d85f6e8098f59492a5d28ee52
Camila Sosa Morales <camisosa@fleek.co>
2023-06-28 12:17:29 -0400

    wip: bunny cdn integrations

--- ui/src/store/features/bunny-cdn/bunny-cdn-client.ts
+++ ui/src/store/features/bunny-cdn/bunny-cdn-client.ts
@@ -2,7 +2,7 @@ import { env } from '@/constants';
 import axios from 'axios';
 
 axios.defaults.baseURL = env.bunnyCDN.url;
-axios.defaults.headers.post['X-Alchemy-Signature'] =
+axios.defaults.headers.post['lambda-signature'] =
   env.bunnyCDN.alchemySignature;
 
 const createPullzone = async (domain: string, targetDomain: string) => {
@@ -27,3 +27,12 @@ export const BunnyCDNClient = {
   verifyPullzone,
 };
 
+
+// export function generateSignature(
+//   body: string, // must be raw string body, not json transformed version of the body
+//   signingKey: string // signing secret key for front-end
+// ): string {
+//   const hmac = crypto.createHmac('sha256', signingKey); // Create a HMAC SHA256 hash using the signing key
+//   hmac.update(body, 'utf8'); // Update the token hash with the request body using utf8
+//   return hmac.digest('hex');
+// }
